f19192b84ecc55b8ea2696d5624af3ab42dd6bf5
xnmng <48902714+xnmng@users.noreply.github.com>
2022-08-24 11:01:15 +0800

    Time: 10 ms (77.00%), Space: 11.6 MB (10.25%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,18 +1,22 @@
 class Solution {
     // returns true if the first argument is less than 
     // (i.e. is ordered before) the second.
-    bool static c(const int& i1, const int& i2) {
-        auto s1{to_string(i1) + to_string(i2)};
-        auto s2{to_string(i2) + to_string(i1)};
-        return s1 > s2;
+    bool static c(const string& s1, const string& s2) {
+        return s1+s2 > s2+s1;
     }
 public:
     string largestNumber(vector<int>& nums) {
         string ans{""};
-        sort(nums.begin(), nums.end(), Solution::c);
-        for (auto i = 0; i < nums.size(); ++i) {
-            ans += to_string(nums[i]);
+        vector<string> v;
+        for (auto i : nums) {
+            v.push_back(to_string(i));
         }
+        sort(v.begin(), v.end(), Solution::c);
+        for (auto i = 0; i < v.size(); ++i) {
+            ans += v[i];
+        }
+        
+        // remove leading zeroes before returning
         for (auto i = 0; i < ans.size(); ++i) {
             if (ans[i] == '0') continue;
             return ans.substr(i);
