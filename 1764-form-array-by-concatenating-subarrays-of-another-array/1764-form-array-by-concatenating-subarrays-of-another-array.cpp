9c7ab6eab9fde32aec7c14e58ea57439ccb165e7
xnmng <48902714+xnmng@users.noreply.github.com>
2022-08-30 00:24:34 +0800

    Time: 46 ms (9.40%), Space: 22.9 MB (5.55%) - LeetHub

---
+++ 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    bool matches(vector<int>& nums, int index, vector<int> v) {
+        if (index + v.size() > nums.size()) return false;
+        for (auto i = 0; i < v.size(); ++i) {
+            if (nums[index+i] != v[i]) return false;
+        }
+        return true;
+    }
+public:
+    bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+        auto cur{0};
+        for (auto i = 0; i < nums.size(); ++i) {
+            // cout << i << "\n";
+            if (cur == groups.size()) return true;
+            if (matches(nums, i, groups[cur])) {
+                // cout << "matched with index " << i << "\n";
+                i += groups[cur].size()-1; // account for ++i in the for-loop
+                ++cur;
+            }
+        }
+        // cout << cur << " " << groups.size() << "\n";
+        return cur == groups.size();
+    }
+};
+
+// [[1,2,3],[1,2,3,4,5]]
+// [1,2,3,4,5,1,2,3]
+// [[-5,0]]
+// [2,0,-2,5,-1,2,4,3,4,-5,-5]
