f0b60dd9b456c7181afd4d80797b1904e3b11704
xnmng <48902714+xnmng@users.noreply.github.com>
2022-08-30 11:34:25 +0800

    Time: 67 ms (87.40%), Space: 48.4 MB (14.80%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,56 @@
+class Trie {
+    vector<Trie*> next;
+    bool isWord;
+public:
+    Trie() : next(vector<Trie*>(26, nullptr)), isWord(false) {}
+    
+    void insert(string word) {
+        auto index{1};
+        if (next[word[0] - 'a'] == nullptr) {
+            next[word[0] - 'a'] = new Trie();
+        }
+        Trie* cur{next[word[0] - 'a']};
+        while (index < word.size()) {
+            if (cur->next[word[index] - 'a'] == nullptr) {
+                cur->next[word[index] - 'a'] = new Trie();
+            }
+            cur = cur->next[word[index] - 'a'];
+            ++index;
+        }
+        cur->isWord = true;
+    }
+    
+    bool search(string word) {
+        auto index{1};
+        auto cur{next[word[0] - 'a']};
+        while (index < word.size()) {
+            if (cur == nullptr) return false;
+            cur = cur->next[word[index] - 'a'];
+            ++index;
+        }
+        if (cur == nullptr) return false;
+        return cur->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        auto index{1};
+        auto cur{next[prefix[0] - 'a']};
+        while (index < prefix.size()) {
+            if (cur == nullptr) return false;
+            cur = cur->next[prefix[index] - 'a'];
+            ++index;
+        }
+        return cur != nullptr;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
+// ["Trie","search"]
+// [[],["a"]]
