df2c1f662d3444061f24655d6604412e08ba882b
xnmng <48902714+xnmng@users.noreply.github.com>
2022-09-10 19:12:47 +0800

    Time: 305 ms (6.55%), Space: 93.3 MB (51.70%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,11 +4,8 @@ public:
         auto ans{0};
         auto cur{prices[0]};
         for (auto i = 1; i < prices.size(); ++i) {
-            if (cur > prices[i]) {
-                cur = prices[i];
-            } else {
-                ans = max(ans, prices[i] - cur);
-            }
+            cur = min(cur, prices[i]);
+            ans = max(ans, prices[i] - cur);
         }
         return ans;
     }
