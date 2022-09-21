56e13ccb97e8987d17fa08437b57c7687e8a1c97
xnmng <48902714+xnmng@users.noreply.github.com>
2022-09-21 12:19:37 +0800

    Time: 315 ms (5.16%), Space: 49.9 MB (16.27%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> ans;
+        auto sum{0};
+        for (auto i : nums) {
+            if (i % 2 == 0) sum += i;
+        }
+        for (auto i : queries) {
+            // even + even = even
+            // even + odd = odd
+            // odd + even = odd
+            // odd + odd = even
+            
+            if (nums[i[1]] % 2 == 0) { // even
+                if (i[0] % 2 == 0) {
+                    sum += i[0];
+                } else {
+                    sum -= nums[i[1]];
+                }
+                nums[i[1]] += i[0];
+            } 
+            else { // odd
+                nums[i[1]] += i[0];
+                if (nums[i[1]] % 2 == 0) { // add odd to odd, now check if index its even
+                    sum += nums[i[1]];
+                }
+            }
+            ans.push_back(sum); 
+        }
+        
+        return ans;
+    }
+};
+
+// [-1,3,-3,9,-6,8,-5]
+// [[-5,1],[10,2],[-6,3],[3,2],[9,5],[7,5],[8,0]]
