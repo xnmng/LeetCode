f12b47c05661daf289209a656010a4bd8873ba2b
xnmng <48902714+xnmng@users.noreply.github.com>
2022-09-26 09:35:42 +0800

    Time: 438 ms (31.02%), Space: 55.9 MB (76.75%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/discuss/1064793/Binary-Search-or-with-Intuition-and-explanation
+class Solution {
+private:
+    bool check(vector<int>& nums, int size, int maxOperations) {
+        auto ans{0};
+        for (auto i : nums) {
+            if (i <= size) continue;
+            ans += (i / size);
+            if (i % size == 0) --ans;
+        }
+        // cout << size << " " << ans << "\n";
+        return ans <= maxOperations;
+    }
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        auto left{1};
+        auto right{INT_MAX};
+        while (left < right) {
+            auto mid{left + (right - left) / 2};
+            // cout << left << " " << right << " " << mid << " " << "\n";
+            if (check(nums, mid, maxOperations)) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+};
