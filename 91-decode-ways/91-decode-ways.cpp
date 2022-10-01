b54ea5cce96036d2ace6c79559795b5886624be0
xnmng <48902714+xnmng@users.noreply.github.com>
2022-10-01 10:51:15 +0800

    Time: 3 ms (63.51%), Space: 6.1 MB (75.45%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,28 @@
+// https://youtu.be/6aEyTjOwlJU
+class Solution {
+    // dp[i] = dp[i+1] + dp[i+2] (only if digit and double digit are valid respectively)
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0') return 0;
+        
+        auto i{static_cast<int>(s.size()-1)};
+        auto dp{0};
+        auto dp1{1};
+        auto dp2{0};
+        
+        while (i >= 0) {
+            dp = 0;
+            if (s[i] != '0') {
+                dp += dp1;
+            }
+            if (i+1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))) {
+                dp += dp2;
+            }
+                
+            --i;
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp;
+    }
+};
