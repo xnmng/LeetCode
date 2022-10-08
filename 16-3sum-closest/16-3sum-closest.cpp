63955e74ad1f7e311839a09ddad6e0877fb1c9c3
xnmng <48902714+xnmng@users.noreply.github.com>
2022-10-08 19:31:01 +0800

    Time: 918 ms (9.88%), Space: 16.3 MB (48.38%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/3sum-closest/discuss/7883/C++-solution-O(n2)-using-sort
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        auto ans{nums[0] + nums[1] + nums[2]};
+        auto last{static_cast<int>(nums.size()-1)};
+        for (auto i = 0; i < nums.size()-2; ++i) {
+            if (ans == target) return target;
+            auto left{i+1};
+            auto right{last};
+            while (left < right) {
+                if (abs(ans - target) > abs(target - nums[i] - nums[left] - nums[right])) {
+                    ans = nums[i] + nums[left] + nums[right];
+                }
+                if (nums[i] + nums[left] + nums[right] > target) {
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+        return ans;
+    }
+};
