4cd73bb2ed208efde3b7e49f22e8b9604fe87c0e
xnmng <48902714+xnmng@users.noreply.github.com>
2022-11-04 10:21:53 +0800

    Time: 203 ms (5.13%), Space: 7.7 MB (61.66%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    bool isVowel(char& c) {
+        return c == 'a' ||
+        c == 'e' ||
+        c == 'i' ||
+        c == 'o' ||
+        c == 'u' ||
+        c == 'A' ||
+        c == 'E' ||
+        c == 'I' ||
+        c == 'O' ||
+        c == 'U';
+    }
+public:
+    string reverseVowels(string s) {
+        auto left{0};
+        auto right{static_cast<int>(s.size()-1)};
+        while (left <= right) {
+            cout << left << " " << right << "\n";
+            if (isVowel(s[left]) && isVowel(s[right])) {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+            else {
+                if (!isVowel(s[left])) ++left;
+                if (!isVowel(s[right])) --right;                
+            }
+        }
+        return s;
+    }
+};
+// "aA"
+// " "
