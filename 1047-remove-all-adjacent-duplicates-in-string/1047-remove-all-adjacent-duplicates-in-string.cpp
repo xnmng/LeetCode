47253146a68afed5cb496e6f4feaea7ffb3cb792
xnmng <48902714+xnmng@users.noreply.github.com>
2022-11-10 10:12:04 +0800

    Time: 78 ms (28.70%), Space: 11.6 MB (24.65%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        deque<char> d;
+        for (auto i : s) {
+            if (d.empty() || d.back() != i) {
+                d.push_back(i);
+            } else {
+                d.pop_back();
+            }
+        }
+        string ans;
+        while (!d.empty()) {
+            ans.push_back(d.front());
+            d.pop_front();
+        }
+        return ans;
+    }
+};
