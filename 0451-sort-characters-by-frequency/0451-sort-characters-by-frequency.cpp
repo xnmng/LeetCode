411361ccfb53666dff59e638d0accc7dee67087b
xnmng <48902714+xnmng@users.noreply.github.com>
2022-12-03 12:20:10 +0800

    Time: 3 ms (99.98%), Space: 8.8 MB (19.08%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<int> v(75);
+        for (auto i : s) {
+            ++v[i - '0'];
+        }
+        vector<pair<int, int>> v2;
+        v2.reserve(52);
+        for (auto i = 0; i < 10; ++i) { // only iterate through char index
+            if (v[i] > 0) v2.emplace_back(v[i], i);
+        }
+        for (auto i = 17; i < 43; ++i) { // only iterate through char index
+            if (v[i] > 0) v2.emplace_back(v[i], i);
+        }
+        for (auto i = 49; i < 75; ++i) { // only iterate through char index
+            if (v[i] > 0) v2.emplace_back(v[i], i);
+        }
+        sort(v2.begin(), v2.end(), greater<pair<int,int>>());
+        string ans;
+        for (auto& [freq, offset] : v2) {
+            ans += string(freq, offset + '0');
+        }
+        return ans;
+    }
+};
+
+// "AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZaabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz0011223344556677889"
+// "2a554442f544asfasssffffasss"
