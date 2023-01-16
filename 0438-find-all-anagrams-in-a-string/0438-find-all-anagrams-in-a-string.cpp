14d2efa2ebd7a741544ad8387a1c89f498c8df78
xnmng <48902714+xnmng@users.noreply.github.com>
2023-01-16 23:28:34 +0800

    Time: 9 ms (96.35%), Space: 8.6 MB (75.22%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    bool check(vector<int>& v) {
+        for (auto i : v) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+public:
+    vector<int> findAnagrams(string s, string p) {
+        if (p.size() > s.size()) return {};
+        vector<int> chars(26, 0);
+        for (auto& i : p) ++chars[i - 'a'];
+        vector<int> ans;
+        for (auto i = 0; i < p.size(); ++i) {
+            --chars[s[i] - 'a'];
+            // for (auto i : chars) {
+            //     cout << i << " ";
+            // }
+            // cout << "\n";
+        }
+        if (check(chars)) ans.push_back(0);
+        for (auto i = p.size(); i < s.size(); ++i) {
+            --chars[s[i] - 'a'];
+            ++chars[s[i - p.size()] - 'a'];
+            if (check(chars)) ans.push_back(i - p.size() + 1);
+            // for (auto i : chars) {
+            //     cout << i << " ";
+            // }
+            // cout << "\n";
+        }
+        return ans;
+    }
+};
+
+// "aaaaaaaaaa"
+// "aaaaaaaaaaaaa"
