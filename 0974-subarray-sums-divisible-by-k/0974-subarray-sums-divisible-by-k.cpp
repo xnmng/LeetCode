34a2118cef6db5592613d4c85108dbda7daf1b6d
xnmng <48902714+xnmng@users.noreply.github.com>
2023-01-19 17:54:00 +0800

    Time: 60 ms (77.50%), Space: 30.2 MB (75.97%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        vector<int> v(k, 0);
+        v[0] = 1; // handle case where first sum itself is a multiple of k
+        auto sum{0};
+        auto ans{0};
+        for (auto i : nums) {
+            sum = ((sum + i) % k + k) % k;
+            ans += v[sum];
+            ++v[sum];
+        }
+        return ans;
+    }
+};
