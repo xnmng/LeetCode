483d1527e95435cdf92815206944a010f6fa6468
xnmng <48902714+xnmng@users.noreply.github.com>
2023-01-24 19:31:16 +0800

    Time: 118 ms (26.03%), Space: 11.3 MB (61.91%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -8,57 +8,34 @@
 // if p[j] == '*', dp[i][j] = dp[i-1][j] || dp[i][j-1]
 
 // https://leetcode.com/problems/wildcard-matching/discuss/1001130/C++-Clean-and-concise-bottom-up-dp-code-with-detailed-explanation-easy-to-understand.
-// class Solution {
-// public:
-//     bool isMatch(string s, string p) {
-//         auto m{s.size()};
-//         auto n{p.size()};
-//         vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        auto m{s.size()};
+        auto n{p.size()};
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
         
-//         dp[0][0] = true;
-//         for (auto i = 1; i <= n && p[i-1] == '*'; ++i) {
-//             dp[0][i] = dp[0][i-1];
-//         }
+        dp[0][0] = true;
+        for (auto i = 1; i <= n && p[i-1] == '*'; ++i) {
+            dp[0][i] = dp[0][i-1];
+        }
 
-//         for (auto i = 1; i <= m; ++i) {
-//             for (auto j = 1; j <= n; ++j) {
-//                 if (p[j-1] == '?' || p[j-1] == s[i-1]) {
-//                     dp[i][j] = dp[i-1][j-1];
-//                 }
-//                 else if (p[j-1] == '*') {
-//                     dp[i][j] = dp[i-1][j] || dp[i][j-1];
-//                 }
-//             }
-//         }
+        for (auto i = 1; i <= m; ++i) {
+            for (auto j = 1; j <= n; ++j) {
+                if (p[j-1] == '?' || p[j-1] == s[i-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if (p[j-1] == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
         
-//         return dp.back().back();
-//     }
-// };
+        return dp.back().back();
+    }
+};
 
 // "adceb"
 // "*a*b"
 // ""
 // "b*"
-
-class Solution {
-public:
-    bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(s.size() + 1, vector(p.size() + 1, false));
-        dp[0][0] = true;
-        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
-            dp[0][j + 1] = true;
-        }
-        
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = 1; j <= p.size(); ++j) {
-                if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
-                }
-            }
-        }
-
-        return dp[s.size()][p.size()];
-    }
-};
