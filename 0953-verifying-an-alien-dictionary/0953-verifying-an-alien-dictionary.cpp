5c466c1bc5e990403269bf64de91f32471b31073
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-02 10:48:03 +0800

    Time: 6 ms (65.24%), Space: 9.6 MB (49.27%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    // true if s2 is after s1 in dict,
+    bool helper(string& s1, string& s2, unordered_map<char,int>& mp) {
+        for (auto i = 0; i < min(s1.size(), s2.size()); ++i) {
+            if (mp[s1[i]] == mp[s2[i]]) {
+                continue;
+            }
+            if (mp[s1[i]] > mp[s2[i]]) {
+                // cout << s1[i] << " is after " << s2[i] << "\n";
+                return false;
+            }
+            if (mp[s1[i]] < mp[s2[i]]) {
+                // cout << s1[i] << " is before " << s2[i] << "\n";
+                return true;
+            }
+        }
+        return s2.size() < s1.size() ? false : true;
+    }
+    
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char,int> mp;
+        for (auto i = 0; i < order.size(); ++i) {
+            mp[order[i]] = i;
+        }
+        for (auto i = 1; i < words.size(); ++i) {
+            if (!helper(words[i-1], words[i], mp)) return false;
+        }
+        return true;
+    }
+};
