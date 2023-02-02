8553867670fb716cfb5b9810dfc5c2dc0941c729
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-02 10:50:11 +0800

    Time: 7 ms (59.43%), Space: 9.7 MB (25.79%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
-    // true if s2 is after s1 in dict,
+    // true if s2 is after s1 in dict
+    // false otherwise
     bool helper(string& s1, string& s2, unordered_map<char,int>& mp) {
         for (auto i = 0; i < min(s1.size(), s2.size()); ++i) {
             if (mp[s1[i]] == mp[s2[i]]) {
