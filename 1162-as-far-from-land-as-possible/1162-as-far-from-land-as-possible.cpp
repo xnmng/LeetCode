633fa9e5f207b387edf4ab47a090d1bf36b8691a
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-10 14:06:44 +0800

    Time: 1647 ms (5.03%), Space: 20 MB (61.81%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        auto n{grid.size()};
+        vector<pair<int,int>> v;
+        for (auto i = 0; i < n; ++i) {
+            for (auto j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) v.emplace_back(i, j);
+            }
+        }
+        int ans{-1};
+        int d;
+        for (auto i = 0; i < n; ++i) {
+            for (auto j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    d = INT_MAX;
+                    for (auto& [x, y] : v) {
+                        d = min(d, abs(x - i) + abs(y - j));
+                    }
+                    // cout << d << "\n";
+                    ans = max(ans, d);
+                }
+            }
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
+
+// [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+// [[1,0,1],[0,0,0],[1,0,1]]
