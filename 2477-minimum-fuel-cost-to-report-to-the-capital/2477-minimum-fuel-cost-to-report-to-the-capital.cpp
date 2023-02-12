7d995857d80467d503a53d114f882df07df75135
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-12 22:19:13 +0800

    Time: 804 ms (35.17%), Space: 229.8 MB (14.25%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int dfs(int cur, vector<vector<int>>& adjlist, int& seats, long long& ans, unordered_set<int>& visited) {
+        visited.insert(cur);
+        // cout << cur << " " << adjlist.size() << "\n";
+        auto sum{1};
+        for (auto i : adjlist[cur]) {
+            if (visited.find(i) != visited.end()) continue;            
+            sum += dfs(i, adjlist, seats, ans, visited);
+        }
+        // cout << cur << " " << sum << " " << (1 + sum/seats) << "\n";
+        ans += 1 + ((sum - 1) / seats);
+        return sum;
+    }
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        vector<vector<int>> adjlist(roads.size()+1);
+        for (auto& i : roads) {
+            adjlist[i[0]].push_back(i[1]);
+            adjlist[i[1]].push_back(i[0]);
+        }
+        auto ans{0ll};
+        unordered_set<int> visited;
+        visited.insert(0);
+        
+        for (auto i : adjlist[0]) {
+            dfs(i, adjlist, seats, ans, visited);
+        }
+        return ans;
+    }
+};
+
+// [[0,1],[1,2],[1,3],[4,2],[5,3],[6,3],[6,7],[8,6],[9,0],[5,10],[11,9],[12,5],[5,13],[8,14],[11,15],[8,16],[17,0],[18,7]]
+// 13
