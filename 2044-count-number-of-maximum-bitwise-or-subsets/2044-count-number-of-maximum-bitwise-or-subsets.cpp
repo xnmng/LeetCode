2055fb46f334914d7e3ee5a7760cb9632e3b2407
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-23 14:02:17 +0900

    Time: 25 ms (68.18%), Space: 8.5 MB (64.18%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void dfs(vector<int>& nums, int index, int cur, int maximum, int& ans) {
+        // cout << index << "\n";
+        if (index == nums.size()) return;
+        if ((cur | nums[index]) == maximum) ++ans;
+        dfs(nums, index + 1, cur | nums[index], maximum, ans);
+        dfs(nums, index + 1, cur, maximum, ans);
+    }
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        auto maximum{0};
+        for (auto i : nums) maximum |= i;
+        // cout << "max: " << maximum << "\n";
+        auto ans{0};
+        dfs(nums, 0, 0, maximum, ans);
+        return ans;
+    }
+};
+
+// class Solution {
+// private:
+//     int dfs(vector<int>& nums, int index, int cur, int maximum) {
+//         // cout << index << "\n";
+//         if (index == nums.size()) return 0;
+//         return ((cur | nums[index]) == maximum)
+//         + dfs(nums, index + 1, cur | nums[index], maximum);
+//         + dfs(nums, index + 1, cur, maximum);
+//     }
+// public:
+//     int countMaxOrSubsets(vector<int>& nums) {
+//         auto maximum{0};
+//         for (auto i : nums) maximum |= i;
+//         // cout << "max: " << maximum << "\n";
+//         // auto ans{0};
+//         return dfs(nums, 0, 0, maximum);
+//     }
+// };
