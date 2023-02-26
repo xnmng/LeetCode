5ae11f2cef6fe726800f791ce59556b061657061
xnmng <48902714+xnmng@users.noreply.github.com>
2023-02-26 15:31:42 +0800

    Time: 18 ms (48.15%), Space: 9 MB (63.20%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,31 @@
+// https://youtu.be/XYi2-LPrwm4
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        auto m{static_cast<int>(word1.size())};
+        auto n{static_cast<int>(word2.size())};
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        for (auto i = 0; i < m+1; ++i) {
+            dp[i][n] = m - i;
+        }
+        for (auto i = 0; i < n+1; ++i) {
+            dp[m][i] = n - i;
+        } 
+        for (auto i = m-1; i >= 0; --i) {
+            for (auto j = n-1; j >= 0; --j) {
+                if (word1[i] == word2[j]) dp[i][j] = dp[i+1][j+1];
+                else {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = min(dp[i][j], 1 + dp[i+1][j+1]);
+                }
+            }
+        }
+        // for (auto i : dp) {
+        //     for (auto j : i) {
+        //         cout << j << " ";
+        //     }
+        //     cout << "\n";
+        // }
+        return dp.front().front();
+    }
+};
