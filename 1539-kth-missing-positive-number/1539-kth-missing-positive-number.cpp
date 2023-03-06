2abf169839734a0c3c316e94e115e7d47ceffa83
xnmng <48902714+xnmng@users.noreply.github.com>
2023-03-06 13:19:47 +0800

    Time: 3 ms (88.86%), Space: 9.4 MB (94.95%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,16 +1,33 @@
+// https://leetcode.com/problems/kth-missing-positive-number/discuss/779999/JavaC++Python-O(logN)
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int next{1};
-        for (auto i : arr) {
-            if (i == next) ++next;
-            else if (k > i - next) {
-                k -= i - next;
-                next = i+1;
-            }
-            else break;
-            // cout << next << " " << k << "\n";
+        auto left{0};
+        auto right{arr.size()};
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            // cout << mid << " " << arr[mid] << "\n";
+            if (arr[mid] - mid - 1 < k) left = mid + 1;
+            else right = mid;
+            // cout << left << " " << right << "\n";
         }
-        return next + k - 1;
+        return left + k;
     }
 };
+
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int next{1};
+//         for (auto i : arr) {
+//             if (i == next) ++next;
+//             else if (k > i - next) {
+//                 k -= i - next;
+//                 next = i+1;
+//             }
+//             else break;
+//         }
+//         return next + k - 1;
+//     }
+// };
