ddd2348ef23eba6c2cfa585fc4387c58783e7d37
xnmng <48902714+xnmng@users.noreply.github.com>
2023-03-15 22:43:21 +0800

    Time: 482 ms (45.56%), Space: 17.4 MB (61.73%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -5,12 +5,9 @@ public:
         vector<vector<bool>> dp(n+1, vector<bool>(n+1, false));
         for (auto i = n-1; i >= 0; --i) {
             for (auto j = n; j >= 0; --j) {
-                // cout << i << " " << j << "\n";
                 if (i >= j) dp[i][j] = true;
                 else if (abs(i - j) <= 1) dp[i][j] = true;
                 else dp[i][j] = (s[i] == s[j-1]) && dp[i+1][j-1];
-                // if (j > i && dp[i][j]) cout << s.substr(i, j-i) << "\n";
-                // if (j > i && dp[i][j]) cout << "[" << i << "," << j << ")\n";
             }
         }
         
@@ -20,22 +17,10 @@ public:
         for (auto i = 1; i < n; ++i) {
             if (dp[0][i]) {
                 for (auto j = i+1; j < n; ++j) {
-                    // cout << i << " " << j << " " << j << " " << n+1 << "\n";
                     if (dp[i][j] && dp[j][n]) return true; // n = 5
                 }
             }
         }
-        // cout << dp[0][1] << " " << dp[1][2] << " " << dp[2][5] << "\n";
-        // for (auto i : dp) {
-        //     for (auto j : i) {
-        //         cout << j << " ";
-        //     }
-        //     cout << "\n";
-        // }
-        // dp[i][i] = false
-        // dp[i][i+1] = true
-        // dp[i][i+2] = true if s[i] == s[i+1]
-        // dp[i][j] = if (s[i] == s[j-1]) && dp[i+1][j-2]
         return false;
     }
 };
