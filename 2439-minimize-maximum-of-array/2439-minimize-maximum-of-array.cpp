4d4ee48aae224b8aa26a67a9a91f6c6936896e22
xnmng <48902714+xnmng@users.noreply.github.com>
2023-04-06 12:36:05 +0800

    Time: 139 ms (74.20%), Space: 71.5 MB (28.67%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,15 +1,13 @@
+// https://youtu.be/AeHMvcKuR0Y
 class Solution {
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        // Initialize answer and the prefix sum.
-        long long answer = 0, prefixSum = 0;
-        
-        // Iterate over nums, update prefix sum and answer.
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum += nums[i];
-            answer = max(answer, (prefixSum + i) / (i + 1));
+        auto sum{0.0};
+        auto ans{INT_MIN * 1.0};
+        for (auto i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            ans = max(ans, ceil(sum / (i+1)));
         }
-        
-        return answer;
+        return ans;
     }
 };
