10d9f776cf313e9ce116133cd453a7dbf0a01944
xnmng <48902714+xnmng@users.noreply.github.com>
2023-04-20 12:27:35 +0800

    Time: 15 ms (20.52%), Space: 23.5 MB (5.25%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// https://youtu.be/FPzLE2L7uHs
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        auto ans{0};
+        deque<pair<TreeNode*, long>> dq;
+        dq.emplace_back(root, 0);
+        pair<TreeNode*, long> cur;
+        long offset;
+        while (!dq.empty()) {
+            ans = max(ans, static_cast<int>(dq.back().second - dq.front().second + 1));
+            deque<pair<TreeNode*, long>> next;
+            offset = dq.front().second * -1;
+            while (!dq.empty()) {
+                cur = dq.front();
+                dq.pop_front();
+                if (cur.first->left) {
+                    next.emplace_back(cur.first->left, offset + cur.second * 2);
+                }
+                if (cur.first->right) {
+                    next.emplace_back(cur.first->right, offset + cur.second * 2 + 1);
+                }
+            }
+            swap(dq, next);
+        }
+        return ans;
+    }
+};
+
+// [0,0,0,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null]
