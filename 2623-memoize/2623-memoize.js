af22caa144c048ea319517e426ad49c11125ea79
xnmng <48902714+xnmng@users.noreply.github.com>
2023-05-13 11:15:07 +0800

    Time: 368 ms (52.44%), Space: 107.4 MB (22.02%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const memoized ={}
+    return (...args) => memoized[args.join()] ?? (memoized[args.join()] = fn(...args)); 
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
