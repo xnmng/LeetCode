6cc3369af5e7ccab59988672f357dda33ded7682
xnmng <48902714+xnmng@users.noreply.github.com>
2023-07-05 18:13:43 +0800

    Time: 44 ms (67.28%), Space: 36.5 MB (65.98%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        auto ans{0};
+        auto prev2{-1};
+        auto prev1{-1};
+        auto prev{-1};
+        for (auto i = 0; i < nums.size(); ++i) {
+            if (nums[i] == 0) {
+                prev2 = prev1;
+                prev1 = prev;
+                prev = i;
+                // cout << prev2 << " " << prev1 << " " << prev << "\n";
+                // if (prev2 == -1) {
+                    ans = max(prev - prev2 - 2, ans);
+                // } else ans = max(prev - prev2 - 2, ans);
+            }
+        }
+        prev2 = prev1;
+        prev1 = prev;
+        prev = nums.size();
+        // cout << prev2 << " " << prev1 << " " << prev << "\n";
+        // cout << prev - prev2 - 3 << "\n";
+        // if (prev2 == -1) {
+            ans = max(prev - prev2 - 2, ans);
+        // } else ans = max(prev - prev2 - 2, ans);
+        return min(ans, static_cast<int>(nums.size()-1)); // handle edge case
+    }
+};
