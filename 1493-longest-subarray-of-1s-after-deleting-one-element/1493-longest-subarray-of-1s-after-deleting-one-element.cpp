552634eef3cb64a3a784bb7996cda80fcfb73656
xnmng <48902714+xnmng@users.noreply.github.com>
2023-07-05 18:33:23 +0800

    Time: 43 ms (71.51%), Space: 36.4 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         auto ans{0};
-        auto prev2{-1};
-        auto prev1{-1};
-        auto prev{-1};
-        for (auto i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) {
-                prev2 = prev1;
-                prev1 = prev;
-                prev = i;
-                // cout << prev2 << " " << prev1 << " " << prev << "\n";
-                ans = max(prev - prev2 - 2, ans);
+        auto cur{0};
+        auto prev{0};
+        for (auto& i : nums) {
+            if (i == 1) ++cur;
+            else {
+                ans = max(ans, cur + prev);
+                prev = cur;
+                cur = 0;
             }
         }
-        prev2 = prev1;
-        prev1 = prev;
-        prev = nums.size();
-        // cout << prev2 << " " << prev1 << " " << prev << "\n";
-        // cout << prev - prev2 - 3 << "\n";
-        ans = max(prev - prev2 - 2, ans);
-        return min(ans, static_cast<int>(nums.size()-1)); // handle edge case
+        ans = max(ans, cur + prev);
+        return min(ans, static_cast<int>(nums.size()-1)); // edge case; arr is all 1's
     }
 };
