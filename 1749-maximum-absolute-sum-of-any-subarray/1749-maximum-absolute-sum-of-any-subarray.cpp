f4c1b4d5e5452c5bb2993d051f18dd6b3f5387ff
xnmng <48902714+xnmng@users.noreply.github.com>
2023-07-24 09:24:27 +0800

    Time: 71 ms (66.03%), Space: 41.4 MB (46.32%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,19 +1,33 @@
+// https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/discuss/1052527/JavaC++Python-O(1)-Space
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        auto ans{0};
-        auto cur{0};
+        auto maxi{0};
+        auto mini{0};
+        auto sum{0};
         for (auto& i : nums) {
-            cur += i;
-            if (cur < 0) cur = 0;
-            else ans = max(ans, cur);
+            sum += i;
+            maxi = max(maxi, sum);
+            mini = min(mini, sum);
         }
-        cur = 0;
-        for (auto& i : nums) {
-            cur += i;
-            if (cur > 0) cur = 0;
-            else ans = max(ans, -1 * cur);
-        }
-        return ans;
+        return maxi - mini;
     }
+    
+    // int maxAbsoluteSum(vector<int>& nums) {
+    //     auto ans{0};
+    //     auto cur{0};
+    //     for (auto& i : nums) {
+    //         cur += i;
+    //         if (cur < 0) cur = 0;
+    //         else ans = max(ans, cur);
+    //     }
+    //     cur = 0;
+    //     for (auto& i : nums) {
+    //         cur += i;
+    //         if (cur > 0) cur = 0;
+    //         else ans = max(ans, -1 * cur);
+    //     }
+    //     return ans;
+    // }
 };
+// [-3,-5,-3,-2,-6,3,10,-10,-8,-3,0,10,3,-5,8,7,-9,-9,5,-8]
