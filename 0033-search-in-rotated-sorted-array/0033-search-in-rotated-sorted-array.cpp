de69c2f2ccbe76b8314eefe06616d3e3e33b97e9
xnmng <48902714+xnmng@users.noreply.github.com>
2023-08-08 18:26:13 +0800

    Time: 7 ms (28.91%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -13,37 +13,37 @@ public:
     determine the sorted half and determine if target is contained inside the sorted half
     repeat to reduce search space
     */
-    // int search(vector<int>& nums, int target) {
-    //     auto left{0};
-    //     auto right{static_cast<int>(nums.size()-1)};
-    //     while (left < right) {
-    //         auto mid{left + (right - left) / 2};
-    //         if (nums[mid] == target) return mid;
-    //         // cout << left << " " << mid << " " << right << "\n";            
-    //         // either nums[left] ... nums[mid] is sorted or nums[mid] ... nums[right]
-    //         if (nums[left] <= nums[mid]) {
-    //             // nums[left] ... nums[mid] is sorted
-    //             // cout << "index " << left << " to " << mid << " is sorted\n";
-    //             // cout << nums[left] << " ... " << nums[mid] << "\n";
-    //             if (nums[mid] >= target && target >= nums[left]) {
-    //                 right = mid - 1;
-    //             } else {
-    //                 left = mid + 1;
-    //             }
-    //         } else {
-    //             // nums[mid] ... nums[right] is sorted
-    //             // cout << "index " << mid << " to " << right << " is sorted\n";
-    //             // cout << nums[mid] << " ... " << nums[right] << "\n";
-    //             if (nums[right] >= target && target >= nums[mid]) {
-    //                 left = mid + 1;
-    //             } else {
-    //                 right = mid - 1;
-    //             }
-    //         }
-    //     }
-    //     // cout << left << "\n";
-    //     return nums[left] == target ? left : -1;
-    // }
+    int search(vector<int>& nums, int target) {
+        auto left{0};
+        auto right{static_cast<int>(nums.size()-1)};
+        while (left < right) {
+            auto mid{left + (right - left) / 2};
+            if (nums[mid] == target) return mid;
+            // cout << left << " " << mid << " " << right << "\n";            
+            // either nums[left] ... nums[mid] is sorted or nums[mid] ... nums[right]
+            if (nums[left] <= nums[mid]) {
+                // nums[left] ... nums[mid] is sorted
+                // cout << "index " << left << " to " << mid << " is sorted\n";
+                // cout << nums[left] << " ... " << nums[mid] << "\n";
+                if (nums[mid] >= target && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // nums[mid] ... nums[right] is sorted
+                // cout << "index " << mid << " to " << right << " is sorted\n";
+                // cout << nums[mid] << " ... " << nums[right] << "\n";
+                if (nums[right] >= target && target >= nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        // cout << left << "\n";
+        return nums[left] == target ? left : -1;
+    }
     
     
     /*
@@ -96,32 +96,32 @@ public:
     define left as pivot index, right as pivot index + n (size of nums)
     at each step find mid and take modulo n
     */
-    int search(vector<int>& nums, int target) {
-        auto n{static_cast<int>(nums.size())};
-        auto left{0};
-        auto right{n-1};
-        while (left < right) {
-            auto mid{left + (right - left) / 2};
-            if (nums[mid] >= nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        // at this step, left will have pivot's value
-        // cout << "pivot index is " << left << "\n";
-        right = n + left;
-        while (left < right) {
-            auto mid{left + (right - left) / 2};
-            if (nums[mid % n] == target) return mid % n;
-            if (nums[mid % n] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return nums[left % n] == target ? left % n : -1;
-    }
+    // int search(vector<int>& nums, int target) {
+    //     auto n{static_cast<int>(nums.size())};
+    //     auto left{0};
+    //     auto right{n-1};
+    //     while (left < right) {
+    //         auto mid{left + (right - left) / 2};
+    //         if (nums[mid] >= nums[right]) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid;
+    //         }
+    //     }
+    //     // at this step, left will have pivot's value
+    //     // cout << "pivot index is " << left << "\n";
+    //     right = n + left;
+    //     while (left < right) {
+    //         auto mid{left + (right - left) / 2};
+    //         if (nums[mid % n] == target) return mid % n;
+    //         if (nums[mid % n] > target) {
+    //             right = mid - 1;
+    //         } else {
+    //             left = mid + 1;
+    //         }
+    //     }
+    //     return nums[left % n] == target ? left % n : -1;
+    // }
 };
 // [3,1]
 // 1
