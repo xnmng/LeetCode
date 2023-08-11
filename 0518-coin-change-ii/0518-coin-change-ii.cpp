587ecc11d64794a980b9732b5bf4c8d728c7f8e6
xnmng <48902714+xnmng@users.noreply.github.com>
2023-08-11 16:55:35 +0800

    Time: 9 ms (87.18%), Space: 7.1 MB (95.27%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        if (amount == 0) return 1;
+        sort(coins.begin(), coins.end());
+        vector<int> dp(amount + 1, 0);
+        for (int& i : coins) {
+            if (i >= dp.size()) continue;
+            ++dp[i];
+            for (int j = i; j < dp.size(); ++j) {
+                dp[j] += dp[j - i];
+            }
+        }
+        // for (auto i : dp) cout << i << " ";
+        // cout << "\n";
+        return dp[amount];
+    }
+};
+// 0
+// [7]
+// 5
+// [1,2,5]
