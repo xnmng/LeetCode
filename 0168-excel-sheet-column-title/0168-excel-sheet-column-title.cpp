873b3c693a53c0347da9c6ee1a04e9f25a20216d
xnmng <48902714+xnmng@users.noreply.github.com>
2023-08-22 09:19:12 +0800

    Time: 2 ms (49.78%), Space: 6 MB (11.31%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        while (columnNumber > 0) {
+            --columnNumber;
+            ans += ('A' + (columnNumber % 26));
+            // cout << ans << "\n";
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
