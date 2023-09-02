f192aeda8a5b7aa61e78097cd38c2c0418e01553
xnmng <48902714+xnmng@users.noreply.github.com>
2023-09-02 11:55:02 +0800

    Time: 154 ms (63.96%), Space: 79.9 MB (60.77%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/extra-characters-in-a-string/solution/
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        vector<int> dp(n+1, INT_MAX);
+        dp[n] = 0;
+        int val;
+        unordered_set<string> uset(dictionary.begin(), dictionary.end());
+        for (auto i = n-1; i >= 0; --i) {
+            dp[i] = dp[i+1] + 1; // base case; this cur letter is extra
+            for (auto j = i; j < n; ++j) {
+                if (uset.find(s.substr(i, j-i+1)) != uset.end()) {
+                    dp[i] = min(dp[i], dp[j+1]);
+                }
+            }
+        }
+        return dp[0];
+    }
+};
