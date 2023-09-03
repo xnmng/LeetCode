16b49b95b2790a2fb8357176244511e0ff91da4f
xnmng <48902714+xnmng@users.noreply.github.com>
2023-09-03 10:04:17 +0800

    Time: 0 ms (100.00%), Space: 6 MB (23.32%) - LeetHub

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long square = (2ll * n - 1) * (2ll * n - 1);
+        long long triangle = 1ll * (n-1) * n / 2;
+        return square - 4 * triangle;
+    }
+};
