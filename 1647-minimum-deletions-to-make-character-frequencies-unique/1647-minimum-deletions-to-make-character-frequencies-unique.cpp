bea86262f228b86c47c2411ef4a185de86ee2fcf
xnmng <48902714+xnmng@users.noreply.github.com>
2023-09-12 09:40:37 +0800

    Time: 51 ms (74.86%), Space: 17.2 MB (77.52%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> v(26, 0);
+        for (char i : s) {
+            ++v[i - 'a'];
+        }
+        sort(v.begin(), v.end());
+        int ans{0};
+        for (int i = 24; i >= 0; --i) {
+            if (v[i] == 0) break;
+            // change v[i] to (v[i+1] - 1), which takes v[i] - 
+            if (v[i] >= v[i+1]) {
+                ans += min(v[i], v[i] - v[i+1] + 1);
+                v[i] = v[i+1] - 1;
+            }
+        }
+        return ans;
+    }
+};
+
