280e236dfd8823ce1addd5bc821a0738f002959a
xnmng <48902714+xnmng@users.noreply.github.com>
2023-09-30 23:17:03 +0800

    Time: 8 ms (71.77%), Space: 11.3 MB (45.36%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        for (auto& i : ops) {
+            m = min(m, i[0]);
+            n = min(n, i[1]);
+        }
+        return m * n;
+    }
+};
