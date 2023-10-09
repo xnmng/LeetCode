af99caf0822b936962c36245c7e700978ea22307
xnmng <48902714+xnmng@users.noreply.github.com>
2023-10-09 13:36:42 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (42.86%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        
+        /*
+            sum of nums [1, ..., n] := n * (n+1) / 2
+            
+            number of elements that are divisible by m from [1, ..., n] = (n / m)
+            
+            first (n / m) elements divisible by m :=
+                m = 1 * m
+                2m = 2 * m
+                ...
+                km = k * m  (where k = n / m)
+                
+            sum of first elements divisible by m := m + ... + km
+                                                    
+                                                    = (m) * (1 + ... + k)
+                                                    
+                                                    = (m) * (k) * (k + 1) / 2
+                                                        
+            ans = nums in [1, ..., n] not divisible by m
+                    - nums in [1, ..., n] divisible by m
+                
+                = nums in [1, ..., n] - nums in [1, ..., n] divisible by m
+                    - nums in [1, ..., n] divisible by m
+                
+                = nums in [1, ..., n] - 2 * nums in [1, ..., n] divisible by m
+
+                = (n * (n+1) / 2) - 2 * ((m) * (k) * (k + 1) / 2)
+                
+                = (n * (n+1) / 2) - 2 * ((m) * (n / m) * ((n / m) + 1) / 2) 
+                    because we know k = n / m
+        */
         return n * (n+1) / 2 - 2 * (m * (n / m) * ((n / m) + 1) / 2);
-        // n / m := number of ints divisible by m
-            
-            
-            // 3 = 1 * 3
-            
-            // 6 = 2 * 3
-            
-            // 9 = 3 * 3
-            
-            // 12 = 4 * 3
-            
-            // 3 * (4 * 5 / 2) = 30
     }
 };
