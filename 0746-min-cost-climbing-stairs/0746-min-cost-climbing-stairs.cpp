cbd49a633892838cfdb52ea6f0b7799b0f0463d1
xnmng <48902714+xnmng@users.noreply.github.com>
2023-10-13 08:56:22 +0800

    Time: 9 ms (19.87%), Space: 13.9 MB (37.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,6 @@
 class Solution {
-    // int dfs(vector<int>& cost, vector<int>& dp, int index) {
-    //     if (index >= cost.size()) return 0;
-    //     if (dp[index] == INT_MAX) {
-    //         dp[index] = cost[index] + min(dfs(cost, dp, index+1), dfs(cost, dp, index+2));
-    //     }
-    //     return dp[index];
-    // }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        // dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        // vector<int> dp(cost.size(), INT_MAX);
-        // dfs(cost, dp, 0);
-        // dfs(cost, dp, 1);
         int prevprev = cost[0];
         int prev = cost[1];
         int temp;
@@ -21,9 +10,5 @@ public:
             prev = temp;
         }
         return min(prevprev, prev);
-        // return min(dp[0], dp[1]);
     }
 };
-
-// 10 15 20 10 12 done
-// 10 15 30 25  
