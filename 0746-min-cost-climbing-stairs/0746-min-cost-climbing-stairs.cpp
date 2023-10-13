9e140ebb309d67e4c1b09633b153995bc46c13ce
xnmng <48902714+xnmng@users.noreply.github.com>
2023-10-13 08:56:06 +0800

    Time: 3 ms (84.96%), Space: 13.9 MB (37.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
-    int dfs(vector<int>& cost, vector<int>& dp, int index) {
-        if (index >= cost.size()) return 0;
-        if (dp[index] == INT_MAX) {
-            dp[index] = cost[index] + min(dfs(cost, dp, index+1), dfs(cost, dp, index+2));
-        }
-        return dp[index];
-    }
+    // int dfs(vector<int>& cost, vector<int>& dp, int index) {
+    //     if (index >= cost.size()) return 0;
+    //     if (dp[index] == INT_MAX) {
+    //         dp[index] = cost[index] + min(dfs(cost, dp, index+1), dfs(cost, dp, index+2));
+    //     }
+    //     return dp[index];
+    // }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         // dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        vector<int> dp(cost.size(), INT_MAX);
-        dfs(cost, dp, 0);
-        dfs(cost, dp, 1);
-        return min(dp[0], dp[1]);
+        // vector<int> dp(cost.size(), INT_MAX);
+        // dfs(cost, dp, 0);
+        // dfs(cost, dp, 1);
+        int prevprev = cost[0];
+        int prev = cost[1];
+        int temp;
+        for (auto i = 2; i < cost.size(); ++i) {
+            temp = cost[i] + min(prevprev, prev);
+            prevprev = prev;
+            prev = temp;
+        }
+        return min(prevprev, prev);
+        // return min(dp[0], dp[1]);
     }
 };
+
+// 10 15 20 10 12 done
+// 10 15 30 25  
