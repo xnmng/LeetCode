c26a505ab8a1a4c6daf7558f9f9e5df2bc42e861
xnmng <48902714+xnmng@users.noreply.github.com>
2023-10-14 22:38:02 +0800

    Time: 3 ms (77.45%), Space: 7 MB (69.52%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int count = 0;
-        for (auto i : s) {
-            if (i == '1') ++count;
+        // int count = 0;
+        // for (auto i : s) {
+        //     if (i == '1') ++count;
+        // }
+        // return string(count-1, '1') + string(s.size() - count, '0') + "1";
+        
+        // more performant; only 1 allocation being made
+        string ans(s.size(), '0');
+        ans.back() = '1';
+        int index = -1;
+        for (char c : s) {
+            if (c == '1') {
+                if (index >= 0) ans[index] = '1';
+                ++index;
+            }
         }
-        return string(count-1, '1') + string(s.size() - count, '0') + "1";
+        return ans;
     }
 };
