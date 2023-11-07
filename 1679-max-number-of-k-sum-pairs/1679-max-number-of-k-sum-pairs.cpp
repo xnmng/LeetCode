4ba7f4bcedb51a66d6a1d73e612afa8231026c38
xnmng <48902714+xnmng@users.noreply.github.com>
2023-11-07 10:33:52 +0800

    Time: 147 ms (14.82%), Space: 73.3 MB (7.92%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int ans = 0;
+        for (auto i : nums) {
+            if (mp[k-i] > 0) {
+                --mp[k-i];
+                ++ans;
+            } else {
+                ++mp[i];
+            }
+        }
+        return ans;
+    }
+};
