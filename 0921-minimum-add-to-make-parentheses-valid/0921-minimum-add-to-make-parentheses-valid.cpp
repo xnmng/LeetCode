3f97ae846d9c117b3cda48b804189551aaba76cc
xnmng <48902714+xnmng@users.noreply.github.com>
2023-11-07 16:09:52 +0800

    Time: 0 ms (100.00%), Space: 6.8 MB (14.35%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0; // number of opening brackets we need
+        int close = 0; // number of closing brackets we need
+        for (auto i : s) {
+            if (i == '(') ++close;
+            // here onwards, we know i == ')'
+            else if (close > 0) --close;
+            else ++open;
+        }
+        return open + close;
+    }
+};
