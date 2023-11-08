a24e849dcb68399cba7db4cb653cc328c8d84b77
xnmng <48902714+xnmng@users.noreply.github.com>
2023-11-08 08:50:05 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (10.74%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -3,7 +3,6 @@ public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
         int diffx = abs(sx - fx);
         int diffy = abs(sy - fy);
-        // cout << diffx << " " << diffy << "\n";
         if (diffx == 0 && diffy == 0) return t != 1; // 0 true, 1 false, >= 2 true
         if (diffy > t || diffx > t) return false;
         return true;
