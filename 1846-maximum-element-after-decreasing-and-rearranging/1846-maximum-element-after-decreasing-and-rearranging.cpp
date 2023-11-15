83cd75f6d1296782462cdabd778f74cd94d1c7a1
xnmng <48902714+xnmng@users.noreply.github.com>
2023-11-15 09:18:11 +0800

    Time: 83 ms (34.70%), Space: 51.6 MB (56.72%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        int n = arr.size();
+        for (auto i = 1; i < n; ++i) {
+            arr[i] = min(arr[i], arr[i-1] + 1);
+        }
+        return arr.back();
+    }
+};
