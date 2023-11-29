d58abe1173c236f2b29fae6bd19e50bd07b58916
xnmng <48902714+xnmng@users.noreply.github.com>
2023-11-29 11:57:04 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (94.23%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,24 @@
 // https://leetcode.com/problems/number-of-1-bits/discuss/1044799/C++-or-Bit-Manipulation-or-4-Approaches-or-Beats-100-0ms-O(1)
+// https://leetcode.com/problems/number-of-1-bits/discuss/55255/C++-Solution:-n-and-(n-1)
 // https://graphics.stanford.edu/~seander/bithacks.html
 class Solution {
 public:
+    // n & (n-1) drops a '1' bit from the number
     int hammingWeight(uint32_t n) {
         int ans = 0;
         while (n != 0) {
-            ans += n % 2;
-            n /= 2;
+            n = n & (n-1);
+            ++ans;
         }
         return ans;
     }
+    
+    // int hammingWeight(uint32_t n) {
+    //     int ans = 0;
+    //     while (n != 0) {
+    //         ans += n % 2;
+    //         n /= 2;
+    //     }
+    //     return ans;
+    // }
 };
