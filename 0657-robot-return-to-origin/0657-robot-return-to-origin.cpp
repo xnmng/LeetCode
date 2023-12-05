7df9ce65d8b1e60fe3b0fd6911754d8c2a7535a0
xnmng <48902714+xnmng@users.noreply.github.com>
2023-12-05 10:52:11 +0800

    Time: 12 ms (40.56%), Space: 8.4 MB (73.45%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for (auto i : moves) {
+            switch (i) {
+                case 'L':
+                    --x;
+                    break;
+                    
+                case 'R':
+                    ++x;
+                    break;
+                    
+                case 'U':
+                    ++y;
+                    break;
+                    
+                case 'D':
+                    --y;
+                    break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+};
