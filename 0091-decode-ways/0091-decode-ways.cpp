f5bf199e19fb1f6ac1c48f7100a0891b80513c80
xnmng <48902714+xnmng@users.noreply.github.com>
2023-12-25 11:20:54 +0800

    Time: 0 ms (100.00%), Space: 6.5 MB (65.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,7 +1,37 @@
+class Solution {
+public:
+    // https://leetcode.com/problems/decode-ways/discuss/30451/Evolve-from-recursion-to-dp
+    // if dont understand this, start from recursive w memo solution
+    int numDecodings(string s) {
+        if (s.front() == '0') return 0;
+        
+        int n = s.size();
+        int cur = 0;
+        int prev = 1; // base case; empty string has 1 way to decode it
+        int prevprev = 0; // doesnt matter since this will not be used for small sized test cases
+        
+        for (int i = n - 1; i >= 0; --i) {
+            // check the case where the number is decoded by itself
+            cur = s[i] == '0' ? 0 : prev;
+            
+            // check the case where the number is decoded with the next number
+            if (s[i] == '1' 
+                || (i+1 < n && s[i] == '2' && s[i+1] < '7')) cur += prevprev;
+            
+            // update the prev and prevprev values (for the next iteration)
+            prevprev = prev;
+            prev = cur;
+        }
+        
+        return cur;
+    }
+};
+
+/*
 // https://youtu.be/6aEyTjOwlJU
 // https://leetcode.com/problems/decode-ways/discuss/1410794/C++Python-From-Top-down-DP-to-Bottom-up-DP-O(1)-Space-Clean-and-Concise
 class Solution {
-    // dp[i] = dp[i+1] + dp[i+2] (only if digit and double digit are valid respectively)
+    // dp[i] = dp[i+1] + dp[i+2] (only if digit and double digit are valid respectively, else dp[i+2] = 0)
 public:
     int numDecodings(string s) {
         if (s[0] == '0') return 0;
@@ -27,3 +57,5 @@ public:
         return dp;
     }
 };
+
+*/
