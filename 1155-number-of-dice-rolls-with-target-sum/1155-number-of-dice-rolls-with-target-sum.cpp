fb844948d05428399e1622a9e3dbb4b2cc1e0a2a
xnmng <48902714+xnmng@users.noreply.github.com>
2023-12-26 09:59:09 +0800

    Time: 11 ms (93.14%), Space: 7.3 MB (77.69%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
         if (target > n*k || target < n) return 0;
-        if (n == 1) return n;
-        
-        vector<long> v(k+1, 1);
-        v[0] = 0;
-        
-        auto cur{2};
-        while (cur <= n) {
-            vector<long> next(v.size()+k, 0);
-            for (auto i = 0; i < v.size(); ++i) {
-                for (auto j = 1; j <= k; ++j) {
-                    if (i+j < next.size()) next[i+j] += v[i];
-                    next[i+j] %= static_cast<long>(1e9 + 7);
+        int modulo = 1e9 + 7;
+        // initialize base case
+        vector<long> v(target+1, 0);
+        for (int i = 1; i <= k && i < target+1; ++i) {
+            v[i] = 1;
+        }
+        int cur = 1;
+        while (cur < n) {
+            vector<long> next(target+1, 0);
+            for (int i = 0; i < target+1; ++i) {
+                for (int j = 1; i+j < target+1 && j <= k; ++j) {
+                    next[i+j] = (next[i+j] + v[i]) % modulo;
                 }
             }
-            
-            // for (auto i : v) cout << i << " ";
-            // cout << "\n";
-            
             v = next;
             ++cur;
         }
-        return static_cast<int>(v[target]);
+        return v.back();
     }
 };
 
