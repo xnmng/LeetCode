437ad88879fa826087d0cf7df98fa5bfb095839d
xnmng <48902714+xnmng@users.noreply.github.com>
2024-01-07 11:06:56 +0800

    Time: 984 ms (39.16%), Space: 285.5 MB (19.17%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,26 +1,3 @@
-// #define LL long long
-// class Solution {
-// public:
-//     int numberOfArithmeticSlices(vector<int>& A) {
-//         int n = A.size();
-//         LL ans = 0;
-//         vector<map<LL, int>> cnt(n);
-//         for (int i = 1; i < n; i++) {
-//             for (int j = 0; j < i; j++) {
-//                 LL delta = (LL)A[i] - (LL)A[j];
-//                 int sum = 0;
-//                 if (cnt[j].find(delta) != cnt[j].end()) {
-//                     sum = cnt[j][delta];
-//                 }
-//                 cnt[i][delta] += sum + 1;
-//                 ans += sum;
-//             }
-//         }
-
-//         return (int)ans;
-//     }
-// };
-
 // https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/1455658/C++JavaPython-DP-with-Picture-explained-Clean-and-Concise
 class Solution {
 public:
@@ -34,6 +11,7 @@ public:
         
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
+                // prevent overflow, cast first number to long long
                 diff = (long long)v[i] - v[j];
                 dp[i][diff] += dp[j][diff] + 1;
                 ans += dp[j][diff];
