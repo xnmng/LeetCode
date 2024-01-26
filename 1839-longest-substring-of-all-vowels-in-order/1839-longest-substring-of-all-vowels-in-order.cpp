388e6007970b1369dc0d8fc42d6d8dc5a428e08b
xnmng <48902714+xnmng@users.noreply.github.com>
2024-01-26 16:51:00 +0800

    Time: 95 ms (47.83%), Space: 28 MB (26.88%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // standard 2 pointer approach; had some struggle implementing
+    int longestBeautifulSubstring(string word) {
+        int count = 1; // start for loop at 1; count starts at 1
+        int comp = 0;
+        int ans = 0;
+        for (int i = 1; i < word.size(); ++i) {
+            if (word[i-1] == word[i]) {
+                ++count;
+            }
+            else if (word[i-1] < word[i]) {
+                ++comp;
+                ++count;
+            } else {
+                comp = 0;
+                count = 1;
+            }
+            if (comp == 4) ans = max(ans, count);
+        }
+        return ans;
+    }
+};
