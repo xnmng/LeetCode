a857f435675b24a065257401e6ce35d9e0c3079f
xnmng <48902714+xnmng@users.noreply.github.com>
2024-01-27 23:15:05 +0800

    Time: 0 ms (100.00%), Space: 10.3 MB (44.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     // keep track of last char seen index, update it accordingly
-    
-    // 2 pointer approach
     int lengthOfLongestSubstring(string s) {
-        vector<bool> v(256, false);
-        int left = 0;
+        vector<int> v(256, -1);
+        int left = -1;
         int right = 0;
-        int count = 0;
         int ans = 0;
         while (right < s.size()) {
-            if (v[s[right]]) {
-                while (left < right && v[s[right]]) {
-                    v[s[left]] = false;
-                    --count;
-                    ++left;
-                }
-            } 
-            v[s[right]] = true;
-            ++count;
-            ans = max(ans, count);
+            if (v[s[right]] > left) left = v[s[right]];
+            v[s[right]] = right;
+            ans = max(ans, right - left);
             ++right;
-                
-            // cout << left << " " << right << " " << ans << " " << count << "\n";
         }
         return ans;
     }
+    
+//     // 2 pointer approach
+//     int lengthOfLongestSubstring(string s) {
+//         vector<bool> v(256, false);
+//         int left = 0;
+//         int right = 0;
+//         int count = 0;
+//         int ans = 0;
+//         while (right < s.size()) {
+//             if (v[s[right]]) {
+//                 while (left < right && v[s[right]]) {
+//                     v[s[left]] = false;
+//                     --count;
+//                     ++left;
+//                 }
+//             } 
+//             v[s[right]] = true;
+//             ++count;
+//             ans = max(ans, count);
+//             ++right;
+                
+//             // cout << left << " " << right << " " << ans << " " << count << "\n";
+//         }
+//         return ans;
+//     }
 };
+
+// " "
+// "abba"
