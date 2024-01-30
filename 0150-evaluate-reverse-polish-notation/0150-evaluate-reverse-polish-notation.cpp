c2b6f1ceb6c34763456b887559249e3a4f020c06
xnmng <48902714+xnmng@users.noreply.github.com>
2024-01-30 12:13:01 +0800

    Time: 6 ms (86.57%), Space: 15.6 MB (5.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    // The reverse polish is a mathematical notation in which operators follow their operands. 
+    // we will get the operands first and then the operators.
+
+    // So, we need to store all the previous operands in order that we receive. 
+    // If we get a operator at anytime, we know that it must be operated on the previous two operands. 
+    // The result that we get will act as operand for future operations and so it must be stored as well. 
+    // We can use a stack to store all the operands. 
+
+    // The algorithm can be summarized as:
+    //     - If the current token is a operand (number), push it into the stack
+    //     - If the token is a operator, pop the top two operands from the stack. 
+    //         Find the result of the operation using the operator and two operands and push the result back into stack
+    //     - Finally, the stack will contain the result of evaluated reverse polish expression.
+    //
+    // We can also use input as stack if we want to (technically) get O(1) space
+    // https://leetcode.com/problems/evaluate-reverse-polish-notation/discuss/1229370/Short-and-Simple-Solution-w-Explanation-or-O(N)-and-O(1)-Space-Solutions
     int evalRPN(vector<string>& tokens) {
         stack<long> s;
         long a;
