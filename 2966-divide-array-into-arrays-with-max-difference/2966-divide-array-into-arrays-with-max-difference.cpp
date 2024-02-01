e05e3bc963271e2f6f58393214112b6342499d38
xnmng <48902714+xnmng@users.noreply.github.com>
2024-02-01 10:24:17 +0800

    Time: 125 ms (99.48%), Space: 119.4 MB (39.64%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // sort the array, since we want each set of 3 elements to be <= k, 
+    // we check every group of 3 elements (i, i+1, i+2) for integer i 
+    // if this can be satisfied
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
