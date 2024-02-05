f85a65cca2a2023befd55bd0e63a57ab672f006d
xnmng <48902714+xnmng@users.noreply.github.com>
2024-02-05 22:59:23 +0800

    Time: 9 ms (58.84%), Space: 10.9 MB (23.79%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,5 +1,32 @@
+// https://youtu.be/kWhy4ZUBdOY?si=gfq3Q4ElBTJDGi7N
 class Solution {
 public:
+    // O(nk) time, O(k) space
+    // (since we notice we only need the prev k values of dp)
+    // circular array optimization (reduce space from O(n) to O(k), use mod k to circle around the dp arr)
+    //
+    // dp[i] records the maximum sum we can get considering A[0] ~ A[i-1]
+    // To get dp[i], we will try to change k last numbers separately to the maximum of them,
+    // where k = 1 to k = K.
+    // int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    //     vector<int> dp(k, 0);
+    //     dp[0] = arr[0]; // base case
+    //     int n = arr.size();
+    //     int curMax;     // store max element value encountered so far
+    //     int indexMax;   // store result for dp[i]; max sum for this given index
+    //     for (int i = 1; i < n; ++i) {
+    //         indexMax = 0; // reset value at the start of this iteration
+    //         curMax = 0; // reset value at the start of this iteration
+    //         for (int j = 1; i <= k && i - j > 0; ++j) {
+    //             curMax = max(curMax, arr[i - j]);
+    //             indexMax = max(indexMax, curMax * j + dp[(i - j) % k]);
+    //         }
+    //         dp[i % k] = indexMax;
+    //     }
+    //     for (auto i : dp) cout << i << " ";
+    //     return dp[(n-1) % k];
+    // }
+    
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         vector<int> dp(arr.size(), 0);
         auto m{INT_MIN};
