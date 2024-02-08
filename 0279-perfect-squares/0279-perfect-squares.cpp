fb52a200fb93c9f35cf36754d7ad0d422968beb8
xnmng <48902714+xnmng@users.noreply.github.com>
2024-02-08 11:32:07 +0800

    Time: 35 ms (93.57%), Space: 10.5 MB (32.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/perfect-squares/discuss/1520447/c++-dp-easy-to-understand
 class Solution {
 public:
+    // dp, initialize base case to perfect square numbers
+    // for a given integer i, check all possible j that are < i
+    // i.e. dp[i] = min(dp[i], dp[j - i] + 1);
     int numSquares(int n) {
         vector<int> v(n+1, INT_MAX);
         for (auto i = 1; i*i < n+1; ++i) {
