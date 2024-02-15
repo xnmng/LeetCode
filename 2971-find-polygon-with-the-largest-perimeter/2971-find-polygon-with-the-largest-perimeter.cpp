b92230cef63e5e50db21ef9cbde3b5e3a11cd924
xnmng <48902714+xnmng@users.noreply.github.com>
2024-02-15 10:17:46 +0800

    Time: 137 ms (82.40%), Space: 83.4 MB (35.28%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    // dont need to check for i >= 2
+    // https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/solution/
     long long largestPerimeter(vector<int>& nums) {
         long long ans = -1;
         sort(nums.begin(), nums.end());
         long long sum = 0;
         for (auto i = 0; i < nums.size(); ++i) {
-            if (i >= 2 && sum > nums[i]) {
+            // check for i >= 2 here is not needed!
+            // when i = 0, the condition will never be true (input elements all > 0)
+            // when i = 1, the condition will never be true (we sorted the elements)
+            // only when i = 2 will there be a possiblity of the condition (sum > nums[i]) returning true
+            if (sum > nums[i]) {
                 ans = sum + nums[i];
             }
             sum += nums[i];
