3f52bef20699f734f3819181c693c5c9cfd510df
xnmng <48902714+xnmng@users.noreply.github.com>
2024-03-02 03:14:56 +0000

    Time: 26 ms (47.82%), Space: 28.4 MB (58.32%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    // 2 pointers, add largest abs value square to ans
+    // dont really need to reverse, just keep track of the desired location then store directly
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
-        ans.reserve(n);
+        vector<int> ans(n);
         int left = 0;
         int right = n-1;
         while (left <= right) {
             if (abs(nums[left]) > abs(nums[right])) {
-                ans.emplace_back(nums[left] * nums[left]);
+                ans[right - left] = (nums[left] * nums[left]);
                 ++left;
             } else {
-                ans.emplace_back(nums[right] * nums[right]);
+                ans[right - left] = (nums[right] * nums[right]);
                 --right;                
             }
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
