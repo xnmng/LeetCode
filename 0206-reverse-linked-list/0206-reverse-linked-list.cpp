5e74e0ab02556952c87ddce85379c0194e090425
xnmng <48902714+xnmng@users.noreply.github.com>
2024-03-21 08:34:03 +0800

    Time: 4 ms (56.53%), Space: 11.4 MB (92.09%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    // TODO: recursive
+    // recursive
     ListNode* reverseList(ListNode* head, ListNode* ans = nullptr) {
         if (!head) return ans;
         ListNode* next = head->next;
