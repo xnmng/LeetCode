82fcbf84e0cd61ca788312a85d6c9e5fdc4a843b
xnmng <48902714+xnmng@users.noreply.github.com>
2024-03-22 08:16:41 +0800

    Time: 182 ms (37.16%), Space: 120.5 MB (55.01%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // O(n) time, O(n) space
+    // bool isPalindrome(ListNode* head) {
+    //     deque<int> dq;
+    //     auto ptr{head};
+    //     while (ptr) {
+    //         dq.push_back(ptr->val);
+    //         ptr = ptr->next;
+    //     }
+    //     while (!dq.empty()) {
+    //         if (dq.front() == dq.back()) {
+    //             dq.pop_front();
+    //             // if deque only has 1 element left
+    //             if (!dq.empty()) {
+    //                 dq.pop_back();
+    //             }
+    //         } else return false;
+    //     }
+    //     return true;
+    // }
+    
+    // O(n) time 
+    //     (3 pass; 1 to find middle, 1/2 to reverse, 1 to check, 1/2 to restore),
+    // O(1) space (with modification)
+    ListNode* reverse(ListNode* head) {
+        if (!head) return nullptr;
+        auto ptr{head->next};
+        ListNode* ans{head};
+        head->next = nullptr;
+        while (ptr) {
+            ListNode* cur = ptr;
+            ptr = ptr->next;
+            cur->next = ans;
+            ans = cur;
+        }
+        return ans;
+    }
+    bool isPalindrome(ListNode* head) {
+        auto slow{head};
+        auto fast{head};
+        while (fast && fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        auto second{slow->next};
+        slow->next = nullptr;
+        
+        auto p2r{reverse(second)};
+        auto p1{head};
+        auto p2{p2r};
+        
+        while (p1 && p2) {
+            if (p1->val != p2->val) {
+                slow->next = reverse(p2r);
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        
+        slow->next = reverse(p2r);
+        return true;
+    }
+};
+
+// 1 2 3 4
+//   s f
+
+// 1 2 3 4 5
+//     s   f
