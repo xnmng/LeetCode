79741584db9e9b738833bc7f04827946bfd59bab
xnmng <48902714+xnmng@users.noreply.github.com>
2024-03-24 14:58:54 +0800

    Time: 80 ms (60.43%), Space: 63.5 MB (93.58%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // clarification: there is 1 repeated number, but it can be repeated >1 times
+    
+    // set -1 flag into input arr
     int findDuplicate(vector<int>& nums) {
         for (int i = 0; i < nums.size(); ++i) {
             if (nums[abs(nums[i])] < 0) return abs(nums[i]);
