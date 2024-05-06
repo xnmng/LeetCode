80fa1a53a68776b147f710508d89d2d8feb1e94a
xnmng <48902714+xnmng@users.noreply.github.com>
2024-05-06 23:46:34 +0800

    Time: 300 ms (23.88%), Space: 165.9 MB (52.57%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -10,8 +10,6 @@
  */
 class Solution {
 public:
-    // stack solution is O(n) time and O(n) space (trivial to implement)
-    // 
     // O(n) time O(1) space (but 2 pass)
     // idea: we notice that if we traverse in reverse order, 
     //          we just need to remove nodes that are smaller than what comes before
@@ -59,3 +57,30 @@ public:
         return ans;
     }
 };
+    // // stack solution is O(n) time and O(n) space (trivial to implement)
+    //     ListNode* removeNodes(ListNode* head) {
+    //     stack<ListNode*> s;
+    //     ListNode* cur;
+    //     while (head) {
+    //         while (!s.empty() && s.top()->val < head->val) {
+    //             cur = s.top();
+    //             // cout << "deleting " << cur->val << "\n";
+    //             s.pop();
+    //             delete(cur);
+    //         }
+    //         s.push(head);
+    //         head = head->next;
+    //     }
+    //     // cout << "ok\n";
+    //     cur = nullptr;
+    //     while (!s.empty()) {
+    //         if (!cur) {
+    //             cur = s.top();
+    //         } else {
+    //             s.top()->next = cur;
+    //             cur = s.top();
+    //         }
+    //         s.pop();
+    //     }
+    //     return cur;
+    // }
