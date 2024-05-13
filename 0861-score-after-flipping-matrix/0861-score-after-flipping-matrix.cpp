7133a12a1b5dbb9eef830a6e707e799898e21f49
xnmng <48902714+xnmng@users.noreply.github.com>
2024-05-13 23:12:59 +0800

    Time: 0 ms (100.00%), Space: 10.3 MB (37.53%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -8,12 +8,43 @@ public:
     // (note: for implementation, dont actually need to perform the flips - O(M) space)
     //
     // if perform the flips, same TC (traverse mn twice), but O(1) space
+    //
+//  // 1 mn traversal solution (O(mn) time, O(m) space)
+//     int matrixScore(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<bool> v(m, false);
+//         for (int i = 0; i < m; ++i) {
+//             if (grid[i][0] == 0) v[i] = true;
+//         }
+        
+//         int ans = 0;
+//         int count = 0;
+//         // iterate across each column
+//         for (int i = 0; i < n; ++i) {
+//             count = 0;
+//             for (int j = 0; j < m; ++j) { // index of v
+//                 if ((v[j] && grid[j][i] == 0) 
+//                     || (!v[j] && grid[j][i] == 1)) ++count;
+//             }
+//             if (count * 2 < m) count = m - count;
+//             ans += count * pow(2, n - i - 1);
+//         }
+//         return ans;
+//     }
+    
+    
+    // 2 mn traversals solution (O(mn) time, O(1) space)
     int matrixScore(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<bool> v(m, false);
         for (int i = 0; i < m; ++i) {
-            if (grid[i][0] == 0) v[i] = true;
+            if (grid[i][0] == 0) {
+                for (int j = 0; j < n; ++j) {
+                    grid[i][j] ^= 1;
+                }
+            }
         }
         
         int ans = 0;
@@ -22,25 +53,11 @@ public:
         for (int i = 0; i < n; ++i) {
             count = 0;
             for (int j = 0; j < m; ++j) { // index of v
-                if ((v[j] && grid[j][i] == 0) 
-                    || (!v[j] && grid[j][i] == 1)) ++count;
+                if (grid[j][i] == 1) ++count;
             }
             if (count * 2 < m) count = m - count;
-            // cout << i << " " << count << "\n";
-            // cout << count * pow(2, n - i - 1) << " " << pow(2, n - i - 1) << "\n\n";
             ans += count * pow(2, n - i - 1);
         }
         return ans;
-        // // form answer and return
-        // int ans = 0;
-        // int cur;
-        // for (auto& i : grid) {
-        //     cur = 0;
-        //     for (auto j : i) {
-        //         cur *= 2;
-        //         cur += j;
-        //     }
-        //     ans += cur;
-        // }
     }
 };
