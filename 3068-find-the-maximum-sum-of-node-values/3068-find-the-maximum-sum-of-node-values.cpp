84b8d69812cd97b01ea87c4a4a0dd095ce47375c
xnmng <48902714+xnmng@users.noreply.github.com>
2024-05-20 00:05:42 +0800

    Time: 191 ms (59.56%), Space: 125.7 MB (67.56%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,11 +1,14 @@
 // tbh, dont really understand the DP solution; problem is greedy in nature
+// greedy solution, not the best O(nlogn) time, O(n) space
+// https://youtu.be/bnBp6_b4GCw?si=lCxcqyraYWqg60eh
+
 class Solution {
 public:
     // we dont actually need to use edges lol
     // why?
     // since a tree is undirected, acyclic and connected, we can always XOR any two nodes with k
     // e.g. in a path from node a to b, 
-    //      all nodes between a and b will be XOR-ed twice, 
+    //      all nodes between (and excluding) a and b will be XOR-ed twice, 
     //      leaving them unchanged
     // 
     // we also know that a XOR k XOR k = a (i.e. applying XOR twice gives us back our intial val)
