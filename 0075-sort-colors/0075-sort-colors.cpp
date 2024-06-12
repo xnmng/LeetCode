444ff2c43e74a029bf2a919852339926bf0f8439
xnmng <48902714+xnmng@users.noreply.github.com>
2024-06-12 18:00:35 +0800

    Time: 0 ms (100.00%), Space: 9.9 MB (34.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // just put 0 in front, 2 behind lmao
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int cur = left;
+        while (cur <= right) {
+            if (nums[cur] == 0) {
+                swap(nums[cur], nums[left]);
+                ++left;
+                ++cur;
+            } else if (nums[cur] == 2) {
+                swap(nums[cur], nums[right]);
+                --right;
+            } else ++cur;
+            // for (auto i : nums) cout << i << " ";
+            // cout << "\n";
+        }
+    }
+};
