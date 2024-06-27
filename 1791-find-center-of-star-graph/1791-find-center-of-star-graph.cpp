2430a6170f26faa57ae76a3601fe0e0843f3a9d8
xnmng <48902714+xnmng@users.noreply.github.com>
2024-06-27 16:04:49 +0800

    Time: 136 ms (61.47%), Space: 70.8 MB (54.65%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
+    // if we assume star graph is valid, can do in constant time
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,int> mp;
-        for (auto i : edges) {
-            ++mp[i[0]];
-            ++mp[i[1]];
-        }
-        for (auto [k,v] : mp) {
-            if (v == edges.size()) return k;
-        }
-        return -1;
+        if (edges[0][0] == edges[1][0] 
+            || edges[0][0] == edges[1][1]) return edges[0][0];
+        return edges[0][1];
     }
+    // int findCenter(vector<vector<int>>& edges) {
+    //     unordered_map<int,int> mp;
+    //     for (auto i : edges) {
+    //         ++mp[i[0]];
+    //         ++mp[i[1]];
+    //     }
+    //     for (auto [k,v] : mp) {
+    //         if (v == edges.size()) return k;
+    //     }
+    //     return -1;
+    // }
 };
