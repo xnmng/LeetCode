15e3f6db2e5e57903b080e4517ad3e477ac6ec39
xnmng <48902714+xnmng@users.noreply.github.com>
2024-07-01 11:31:09 +0800

    Time: 6 ms (25.20%), Space: 10.6 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        for (auto i = 2; i < arr.size(); ++i) {
-            if (arr[i] % 2 == 1
-               && arr[i-1] % 2 == 1
-               && arr[i-2] % 2 == 1) return true;
+        for (int i = 2; i < arr.size(); ++i) {
+            if (arr[i] % 2 == 1 &&
+                arr[i-1] % 2 == 1 &&
+                arr[i-2] % 2 == 1) {
+                return true;
+            }
         }
         return false;
     }
