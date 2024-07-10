7edb137af65dac866cf28e92381eb2536254d751
xnmng <48902714+xnmng@users.noreply.github.com>
2024-07-10 12:07:52 +0800

    Time: 9 ms (22.38%), Space: 13.9 MB (84.88%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int count = 0;
-        for (auto& i : logs) {
-            if (i == "../") {
-                count = max(count-1, 0);
-            } else if (i != "./") {
-                ++count;
-            }
+        for (auto& log : logs) {
+            if (log == "../") count = max(count - 1, 0);
+            else if (log == "./") continue;
+            else ++count;
         }
         return count;
     }
