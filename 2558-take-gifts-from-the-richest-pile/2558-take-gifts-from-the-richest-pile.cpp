afa878f6c18a9218379682b46b2d6d24c323b62a
xnmng <48902714+xnmng@users.noreply.github.com>
2024-08-01 12:36:49 +0800

    Time: 9 ms (35.64%), Space: 13.3 MB (14.19%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq(gifts.begin(), gifts.end());
+        long long ans = 0;
+        while (k > 0) {
+            --k;
+            int cur = pq.top();
+            pq.pop();
+            pq.push((int) sqrt(cur));
+        }
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+        return ans;
+    }
+};
