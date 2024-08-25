e111ef63aa33743ad96d80924b5e15a6d921d7b3
xnmng <48902714+xnmng@users.noreply.github.com>
2024-08-25 13:19:11 +0800

    Time: 5 ms (7.16%), Space: 10.1 MB (60.43%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // iterative solution
+    // https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/5686926/Iterative-+-recursive-or-100-or-C++-or-simple
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ans;
+        TreeNode* prev = nullptr;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            auto cur = s.top();
+            s.pop();
+            ans.emplace_back(cur->val);
+            if (cur->left) s.push(cur->left);
+            if (cur->right) s.push(cur->right);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    
+    // recursive solution
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     if (!root) return {};
+    //     vector<int> ans{postorderTraversal(root->left)};
+    //     auto right{postorderTraversal(root->right)};
+    //     ans.insert(ans.end(), right.begin(), right.end());
+    //     ans.emplace_back(root->val);
+    //     return ans;
+    // }
+};
