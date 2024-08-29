ba350393767066098e808c8eed4eaeefcbdcc5e0
xnmng <48902714+xnmng@users.noreply.github.com>
2024-08-29 17:41:46 +0800

    Time: 45 ms (40.49%), Space: 25 MB (26.62%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,21 +1,53 @@
-// https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/discuss/197851/C++-DFS
 class Solution {
 public:
-    int dfs(unordered_map<int, vector<int>> &rows, unordered_map<int, vector<int>> &cols, unordered_set<int> &v_rows, int row) {
-  auto res = 0;
-  if (v_rows.insert(row).second) {
-    res += rows[row].size();
-    for (auto c : rows[row])
-      for (auto r : cols[c]) res += dfs(rows, cols, v_rows, r);
-  }
-  return res;
-}
-int removeStones(vector<vector<int>>& stones) {
-  unordered_map<int, vector<int>> rows, cols;
-  unordered_set<int> v_rows;
-  auto res = 0;
-  for (auto s : stones) rows[s[0]].push_back(s[1]), cols[s[1]].push_back(s[0]);
-  for (auto r : rows) res += max(0, dfs(rows, cols, v_rows, r.first) - 1);
-  return res;
-}
+    // dfs would work
+    //
+    // TODO: solve using union find disjoint set
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+
+        unordered_map<int,vector<int>> cols; // coordinate to index (stones[i])
+        unordered_map<int,vector<int>> rows; // coordinate to index (stones[i])
+        
+        vector<bool> visited(n, false);
+        
+        for (int i = 0; i < n; ++i) {
+            // cout << "pre " << stones[i][0] << " " << stones[i][1] << "\n";
+            // cout << "col " << stones[i][1] << " add " << i << "\n";
+            // cout << "row " << stones[i][0] << " add " << i << "\n";
+            rows[stones[i][0]].emplace_back(i);
+            cols[stones[i][1]].emplace_back(i);
+        }
+        
+        int count = 0; // number of components
+        
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                // cout << "dfs " << i << "\n";
+                dfs(i, stones, cols, rows, visited);
+                ++count;
+            }
+        }
+        
+        return n - count;
+    }
+    
+    void dfs(int index, vector<vector<int>>& stones,
+             unordered_map<int, vector<int>>& cols, 
+             unordered_map<int, vector<int>>& rows, 
+             vector<bool>& visited) {
+        // cout << "at " << stones[index][0] << " " << stones[index][1] << "\n";
+        if (!visited[index]) {
+            visited[index] = true;
+            for (int i : rows[stones[index][0]]) {
+                if (!visited[i]) dfs(i, stones, cols, rows, visited);
+            }
+            for (int i : cols[stones[index][1]]) {
+                if (!visited[i]) dfs(i, stones, cols, rows, visited);
+            }
+
+        }
+    }
 };
+// [[0,1],[1,2],[1,3],[3,3],[2,3],[0,2]]
+// [[0,1],[1,0]]
