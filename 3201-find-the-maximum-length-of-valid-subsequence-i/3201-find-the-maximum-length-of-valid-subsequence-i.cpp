cd1e6727d5ea61dc5d95881fabcd86ee2ab01e94
xnmng <48902714+xnmng@users.noreply.github.com>
2024-09-20 00:47:22 +0800

    Time: 110 ms (67.14%), Space: 95.2 MB (44.62%) - LeetHub

---
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    // simulate
+    // recall:
+    //      odd + odd = even
+    //      even + even = even
+    //      odd + even = odd
+    //      even + odd = odd
+    //
+    // this forms 3 sequences:
+    //      odd, odd, odd, odd, ...
+    //      odd, even, odd, even ...
+    //      even, even, even, even ...
+    //
+    // check all 3 sequences and return longest one
+    int maximumLength(vector<int>& nums) {
+        int allOdd;
+        int allEven;
+        int alternating = 1;
+        bool isOdd; // flag indicating what next number in sequence should be
+        
+        // init base case
+        if (nums[0] % 2 == 0) {
+            allOdd = 0;
+            allEven = 1;
+            isOdd = true;
+        } else {
+            allOdd = 1;
+            allEven = 0;
+            isOdd = false; 
+        }
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] % 2 == 0) {
+                ++allEven;
+                if (!isOdd) {
+                    ++alternating;
+                    isOdd ^= true;
+                }
+            }
+            else {
+                ++allOdd;
+                if (isOdd) {
+                    ++alternating;
+                    isOdd ^= true;
+                }
+            }
+        }
+        
+        return max(allOdd, max(allEven, alternating));
+    }
+};
