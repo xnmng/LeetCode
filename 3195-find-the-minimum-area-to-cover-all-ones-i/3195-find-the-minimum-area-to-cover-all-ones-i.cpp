8010c2c98053bc56c9dbf40e51854087f7e48b9c
xnmng <48902714+xnmng@users.noreply.github.com>
2024-09-21 02:22:38 +0800

    Time: 259 ms (71.73%), Space: 132.9 MB (44.12%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // since we need 1 rectangle to cover all the 1's and we want minimum area,
+    // just make the rectangle just big enough to cover all the 1's
+    // to do this, we just maintain the bounds of each 1, 
+    // take the max bound (up, down, left, right) of each
+    // and find the area of this rectangle drawn by these 4 bounds
     int minimumArea(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -17,7 +22,6 @@ public:
                 }
             }
         }
-        // cout << left << " " << right << " " << up << " " << down << "\n";
         return (down - up + 1) * (right - left + 1);
     }
 };
