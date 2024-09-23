e0b0fe8dbc988df4a53951da6a74ecc7e79cbd75
xnmng <48902714+xnmng@users.noreply.github.com>
2024-09-24 01:18:19 +0800

    Time: 0 ms (100.00%), Space: 26.6 MB (22.10%) - LeetHub

---
+++ 3206-alternating-groups-i/3206-alternating-groups-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // copy paste part ii here
+    
+    // sliding window, maintain counts of alternating colours 
+    // 1 group of size k should have k-1 adjacent differences; set count to 1 initially for easy comparison
+    int numberOfAlternatingGroups(vector<int>& colors, int k = 3) {
+        int count = 1;
+        int n = colors.size();
+        int ans = 0;
+        for (int i = 1; i < n + k; ++i) {
+            // cout << i << " " << i % n << " " << (i-1) % n << " " << (i >= k) << " " << (i-k) % n << " " << (i-k+1) % n << "\n";
+            // cout << i << " " << count << "\n";
+            if (count == k) ++ans;
+            if (colors[i % n] != colors[(i-1) % n]) ++count;
+            if (i >= k && colors[(i-k) % n] != colors[(i-k+1) % n]) --count;
+        }
+        
+        return ans;
+    }
+};
