004ab9b411bb21fd414f945256e645f6ae1db390
xnmng <48902714+xnmng@users.noreply.github.com>
2024-09-25 12:13:11 +0800

    Time: 501 ms (85.78%), Space: 704 MB (71.07%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    // use a trie to store the strings and each char frequency
+    // iterate throug again to compute the score for a given string
+    // O(nk) time 2 pass, O(nk) space
+    // where n is the number of words and k is the number of chars for each word
+    vector<int> sumPrefixScores(vector<string>& words) {
+        vector<int> ans;
+        ans.reserve(words.size());
+        Node* root = new Node();
+        Node* cur;
+        for (auto& i : words) {
+            cur = root;
+            for (char c : i) {
+                if (cur->next[c - 'a'] == nullptr) {
+                    cur->next[c - 'a'] = new Node();
+                }
+                ++cur->next[c - 'a']->freq;
+                cur = cur->next[c - 'a'];
+            }            
+        }
+
+        for (auto& i : words) {
+            cur = root;
+            int count = 0;
+            int index = 0;
+            for (char c : i) {
+                // when counting, if we enounter 1 as the freq, 
+                // we know theres only 1 word with this value, 
+                // can calculate and return early
+                if (cur->next[c - 'a']->freq == 1) {
+                    count -= index;
+                    count += i.size();
+                    break;
+                }
+                count += cur->next[c - 'a']->freq;
+                cur = cur->next[c - 'a'];
+                ++index;
+            }
+            ans.emplace_back(count);
+        }
+        return ans;
+    }
+    
+    struct Node {
+        int freq;
+        Node* next[26];
+        Node() : freq(0), next(nullptr) {}
+        
+        ~Node() {
+            for (auto& child : next) {
+                delete child;  // Recursively delete child nodes
+            }
+        }
+    };
+
+    void deleteTrie(Node* node) {
+        if (node) {
+            for (auto& child : node->next) {
+                deleteTrie(child);
+            }
+            delete node;
+        }
+    }
+};
