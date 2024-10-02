2dd8d2b07dd166f2eaa1b33aecbbcea6957caaf5
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-02 10:48:19 +0800

    Time: 103 ms (26.95%), Space: 42.8 MB (72.64%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     // sort using map, generate ans vector and return
+    // can reduce down to O(k) space (where k is the number of unique elements in arr if we traverse twice)
+    // O(N) time, O(k) space
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,vector<int>> mp; // value to index
+        map<int,int> mp; // value to index
         int n = arr.size();
         for (int i = 0; i < n; ++i) {
-            mp[arr[i]].emplace_back(i);
+            mp[arr[i]];
         }
-        vector<int> ans(n);
+        vector<int> ans;
+        ans.reserve(n);
         int rank = 1;
-        for (auto [k,v] : mp) {
-            for (int i : v) {
-                ans[i] = rank;
-            }
+        for (auto& [k,v] : mp) {
+            v = rank;
             ++rank;
+        }
+        for (int i = 0; i < n; ++i) {
+            ans.emplace_back(mp[arr[i]]);
         }
         return ans;
     }
+    
+    // O(N) time, O(N) space
+    // vector<int> arrayRankTransform(vector<int>& arr) {
+    //     map<int,vector<int>> mp; // value to index
+    //     int n = arr.size();
+    //     for (int i = 0; i < n; ++i) {
+    //         mp[arr[i]].emplace_back(i);
+    //     }
+    //     vector<int> ans(n);
+    //     int rank = 1;
+    //     for (auto [k,v] : mp) {
+    //         for (int i : v) {
+    //             ans[i] = rank;
+    //         }
+    //         ++rank;
+    //     }
+    //     return ans;
+    // }
 };
