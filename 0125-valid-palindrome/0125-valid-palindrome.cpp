7bef292b4a40b92b4792d3185b2c6376feb91c72
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-08 14:06:31 +0800

    Time: 9 ms (22.25%), Space: 8.9 MB (83.07%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right) {
+            if (!isalpha(s[left]) && !isdigit(s[left])) ++left;
+            else if (!isalpha(s[right]) && !isdigit(s[right])) --right;
+            else if (tolower(s[left]) == tolower(s[right])) {
+                ++left;
+                --right;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
