85c3085d1993353ddd65f442e97e31381f044e31
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-08 16:00:49 +0800

    Time: 78 ms (51.91%), Space: 70.5 MB (71.80%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // kadanes algo
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int ans = INT_MIN;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            ans = max(ans, sum);
+            if (sum < 0) sum = 0;
+        }
+        return ans;
+    }
+};
