5bf6fe4422d6aec517ad3dc7c6ee1c4b143c7f12
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-08 14:03:18 +0800

    Time: 99 ms (8.76%), Space: 96.2 MB (73.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // suppose today i want to sell the stock, how much would i make?
+    // store the smallest price encountered so far, then calculate
     int maxProfit(vector<int>& prices) {
         auto ans{0};
         auto cur{prices[0]};
