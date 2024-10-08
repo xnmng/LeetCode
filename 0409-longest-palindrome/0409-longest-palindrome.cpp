3affaa70f885f232aceadc81869275c6dcb4332e
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-08 18:09:48 +0800

    Time: 5 ms (30.47%), Space: 8 MB (93.24%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+    // the longest palindrome we have is to take pairs of elements and (if applicable) 1 odd char
+    // O(n) time, O(1) space
     int longestPalindrome(string s) {
-        unordered_map<char,int> mp;
+        vector<bool> v(256, 0);
+        int ans = 0;
+        int count = 0; // store number of true inside vector v
+        
         for (char c : s) {
-            ++mp[c];
+            if (v[c]) {
+                ans += 2;
+                count -= 2; // to cancel out the ++count below since we are removing 1
+            }
+            ++count;
+            v[c] = v[c] ^ true;
         }
-        bool hasOdd{false};
-        int ans{0};
-        for (auto& [k, v] : mp) {
-            ans += v/2 * 2;
-            if (v % 2 == 1) hasOdd = true;
-        }
-        return ans + (hasOdd ? 1 : 0);
+        
+        return ans + (count > 0 ? 1 : 0);
     }
+
+    
+//     // using ump to count frequencies (O(k) space; k is number of unique chars)
+//     int longestPalindrome(string s) {
+//         unordered_map<char,int> mp;
+//         for (char c : s) {
+//             ++mp[c];
+//         }
+//         bool hasOdd{false};
+//         int ans{0};
+//         for (auto& [k, v] : mp) {
+//             ans += v/2 * 2;
+//             if (v % 2 == 1) hasOdd = true;
+//         }
+//         return ans + (hasOdd ? 1 : 0);
+//     }
 };
