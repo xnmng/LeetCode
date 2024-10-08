72c147831a241fbbf839d18a0b5f10ffddaefe22
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-08 13:56:25 +0800

    Time: 4 ms (93.85%), Space: 14.4 MB (21.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     // either:
-    // O(n^2) time, O(1) space
-    // O(n) time, O(n) space
+    // O(n^2) time, O(1) space (two pointers, keep moving left forward and check each possible right)
+    // O(n) time, O(n) space (umap and find its complement)
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp; // <num, index>
         int n = nums.size();
