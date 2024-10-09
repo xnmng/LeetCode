794a36282f2317f73649ce365a1d187c6a05ee3d
xnmng <48902714+xnmng@users.noreply.github.com>
2024-10-09 16:42:25 +0800

    Time: 3 ms (97.94%), Space: 20.4 MB (83.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,33 +11,42 @@
  */
 
 
-// do in order traversal
+// in order traversal solution
+//
+// just traverse in order and make sure nodes are increasing
+//
 // https://leetcode.com/problems/validate-binary-search-tree/discuss/32104/C++-in-order-traversal-and-please-do-not-rely-on-buggy-INT_MAX-INT_MIN-solutions-any-more
 class Solution {
 public:
-//     bool isValidBST(TreeNode* root) {
-//         auto [_, flag] = helper(root);
-//         return flag;
-//     }
-    
-//     // <prev_value, is_bst>
-//     pair<int, bool> helper(TreeNode* root) {
-        
-//     }
-// };
-    
-    
-    // bad practice to rely on INT_MAX and INT_MIN to compare, compare using the nodes themselves
-    bool helper(TreeNode* root, TreeNode* left = nullptr, TreeNode* right = nullptr) {
-        if (!root) return true;
-        if (left && left->val >= root->val) return false;
-        if (right && right->val <= root->val) return false;
-        return helper(root->left, left, root) 
-            && helper(root->right, root, right);
-    }
     bool isValidBST(TreeNode* root) {
+        TreeNode* prev = nullptr;
+        return helper(root, prev);
+    }
+    
+    bool helper(TreeNode* root, TreeNode*& prev) {
         if (!root) return true;
-        return helper(root->left, nullptr, root)
-            && helper(root->right, root, nullptr);
+        if (!helper(root->left, prev)) return false;
+        if (prev && root->val <= prev->val) return false;
+        prev = root;
+        return helper(root->right, prev);
     }
 };
+    
+    
+//     // recursive solution
+//     // bad practice to rely on INT_MAX and INT_MIN to compare, compare using the nodes themselves
+//     bool helper(TreeNode* root, TreeNode* left = nullptr, TreeNode* right = nullptr) {
+//         if (!root) return true;
+//         if (left && left->val >= root->val) return false;
+//         if (right && right->val <= root->val) return false;
+//         return helper(root->left, left, root) 
+//             && helper(root->right, root, right);
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         if (!root) return true;
+//         return helper(root->left, nullptr, root)
+//             && helper(root->right, root, nullptr);
+//     }
+// };
+
+// [1,null,1]
