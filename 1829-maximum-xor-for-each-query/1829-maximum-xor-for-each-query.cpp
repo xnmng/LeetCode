82a8db92a432b0b5f284d1cf363cf7042701aaa4
xnmng <48902714+xnmng@users.noreply.github.com>
2024-11-08 10:00:19 +0800

    Time: 5 ms (40.95%), Space: 95.6 MB (53.70%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+        vector<int> ans;
+        ans.reserve(n);
+        int cur = 0;
+        for (int i : nums) cur ^= i;
+        int k = pow(2, maximumBit) - 1;
+        for (int i = n-1; i >= 0; --i) {
+            ans.emplace_back(cur ^ k);
+            cur ^= nums[i];
+        }
+        return ans;
+    }
+};
