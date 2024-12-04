c425593144ebdda1da31a7fc2019f4661632ca9d
xnmng <48902714+xnmng@users.noreply.github.com>
2024-12-04 10:56:49 +0800

    Time: 0 ms (100%), Space: 17.1 MB (7.53%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // a little confused on why greedy defo works
+    bool canMakeSubsequence(string str1, string str2) {
+        int p1 = 0;
+        int p2 = 0;
+        int m = str1.size();
+        int n = str2.size();
+
+        while (p1 < m && p2 < n) {
+            if (str1[p1] == str2[p2] ||
+                str1[p1] + 1 == str2[p2] || 
+                (str1[p1] == 'z' && str2[p2] == 'a')) {
+                    ++p2;
+            }
+            ++p1;
+        }
+        return p2 == n;
+    }
+};
