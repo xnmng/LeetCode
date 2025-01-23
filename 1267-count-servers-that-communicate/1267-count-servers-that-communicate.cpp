6158ba2bc55186cb8b79c77e395316d82facd04f
xnmng <48902714+xnmng@users.noreply.github.com>
2025-01-23 08:08:23 +0800

    Time: 0 ms (100%), Space: 26.6 MB (73.49%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> v1(m, false);
+        vector<int> v2(n, false);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    ++v1[i]; 
+                    ++v2[j];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (v1[i] > 1 || v2[j] > 1)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
