0f1182ad789bd6f0d8c4596065031f22d84a976d
xnmng <48902714+xnmng@users.noreply.github.com>
2025-07-15 13:01:05 +0800

    Time: 2 ms (13.35%), Space: 7.8 MB (97.82%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     bool isValid(string word) {
         if (word.size() < 3) return false;
-        bool vowel = false;
-        bool consonant = false;
-        for (auto i : word) {
-            i = tolower(i);
-            if (i == '@' || i == '#' || i == '$') return false;
-            else if (isdigit(i)) continue;
-            else if (i == 'a' 
-                    || i == 'e'
-                    || i == 'i'
-                    || i == 'o'
-                    || i == 'u') vowel = true;
-            else consonant = true;
+        bool hasConsonant = false;
+        bool hasVowel = false;
+        for (char c : word) {
+            if (isdigit(c)) continue;
+            if (!isalpha(c)) return false;
+            // is alphabet
+            c = tolower(c);
+            if (isvowel(c)) hasVowel = true;
+            else hasConsonant = true;
         }
-        return vowel && consonant;
+        return hasVowel && hasConsonant;
+    }
+
+    bool isvowel(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     }
 };
