e15d14ed4e031aef4c640712c23440e58324ceed
xnmng <48902714+xnmng@users.noreply.github.com>
2025-09-18 15:54:38 +0800

    Time: 219 ms (83.13%), Space: 366.3 MB (67.19%) - LeetHub

--- 3408-design-task-manager/3408-design-task-manager.cpp
+++ 3408-design-task-manager/3408-design-task-manager.cpp
@@ -1,39 +1,52 @@
 class TaskManager {
-  priority_queue<pair<int, int>> tasks;
-  unordered_map<int, int> taskPriority;
-  unordered_map<int, int> taskOwner;
-
 public:
-  TaskManager(vector<vector<int>>& tasks) {
-    for (const auto& task : tasks) {
-      add(task[0], task[1], task[2]);
+    unordered_map<int,int> mp1; // taskid -> priority
+    unordered_map<int,int> mp2; // taskid -> userid
+    priority_queue<pair<int,int>> pq; // sort tasks by (priority, taskid)
+
+    TaskManager(vector<vector<int>>& tasks) {
+        for (auto& i : tasks) {
+            add(i[0], i[1], i[2]);
+        }
     }
-  }
     
-  void add(int userId, int taskId, int priority) {
-    tasks.push({priority, taskId});
-    taskPriority[taskId] = priority;
-    taskOwner[taskId] = userId;
-  }
-    
-  void edit(int taskId, int newPriority) {
-    tasks.push({newPriority, taskId});
-    taskPriority[taskId] = newPriority;
-  }
-    
-  void rmv(int taskId) {
-    taskPriority[taskId] = -1;
-  }
-    
-  int execTop() {
-    while (!tasks.empty()) {
-      const auto task = tasks.top();
-      tasks.pop();
-      if (task.first == taskPriority[task.second]) {
-        taskPriority[task.second] = -1;
-        return taskOwner[task.second];
-      }
+    // add into data structures for bookkeeping
+    void add(int userId, int taskId, int priority) {
+        pq.push({priority, taskId});
+        mp1[taskId] = priority;
+        mp2[taskId] = userId;
     }
-    return -1;
-  }
+    
+    // insert new entry into pq, umap keeps track of correct priority
+    void edit(int taskId, int newPriority) {
+        pq.push({newPriority, taskId});
+        mp1[taskId] = newPriority;
+    }
+    
+    // set task priority to -1 (sentinel value)
+    void rmv(int taskId) {
+        mp1[taskId] = -1;
+    }
+    
+    // keep popping until we find a task with matching priority inside umap
+    int execTop() {
+        while(!pq.empty()) {
+            auto [priority, taskId] = pq.top();
+            pq.pop();
+            if (priority == mp1[taskId]) {
+                mp1[taskId] = -1;
+                return mp2[taskId];
+            }
+        }
+        return -1;
+    }
 };
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
