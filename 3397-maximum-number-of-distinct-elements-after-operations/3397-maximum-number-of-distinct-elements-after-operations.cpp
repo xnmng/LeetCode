e205a2796b0d675dbbf7a9ba0d654c27edbca659
xnmng <48902714+xnmng@users.noreply.github.com>
2025-10-18 13:04:52 +0800

    Time: 123 ms (49.53%), Space: 97.7 MB (46.06%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -3,19 +3,22 @@ public:
     // trick: to optimize from brute force checking [i - k, i + k]
     // keep track of prev, if prev doesnt exist, smallest possible is i-k
     // otherwise, smallest possible is i-k+1 or i2-k
+    // this way, we can check if next element is possible in O(1), not O(k) time
+    // 
+    // O(nlogn) time, O(sort) space
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        unordered_set<int> s;
+        int ans = 0;
         int prev = INT_MAX;
         for (int i : nums) {
             if (prev == INT_MAX) {
-                s.insert(i - k);
+                ++ans;
                 prev = i - k;
             }
             else {
                 if (prev + 1 > i + k) continue;
                 prev = max(prev + 1, i - k);
-                s.insert(prev);
+                ++ans;
             }
             // if (s.contains(i + k)) continue;
             // for (int j = -k; j <= k; ++j) {
@@ -25,6 +28,6 @@ public:
             //     }
             // }
         }
-        return s.size();
+        return ans;
     }
 };
