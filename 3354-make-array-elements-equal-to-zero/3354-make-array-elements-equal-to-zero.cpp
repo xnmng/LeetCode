5e5332e7f7487df117d20293b53fac0456f3d14a
xnmng <48902714+xnmng@users.noreply.github.com>
2025-10-28 15:57:26 +0800

    Time: 7 ms (44.95%), Space: 22 MB (80.73%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    // brute force (TODO: might have more efficient solution, but this suffices for contest)
     int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
         int ans = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == 0 && simulate(nums, i, 1)) ++ans; 
-            if (nums[i] == 0 && simulate(nums, i, -1)) ++ans; 
-        }
-        return ans;
-    }
-
-    bool simulate(vector<int> nums, int i, int dir) {
-        int n = nums.size();
-        while (i >= 0 && i < n) {
-            if (nums[i] == 0) i += dir;
-            else {
-                --nums[i];
-                dir = -dir;
-                i += dir;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        int rightSum = sum;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
+                    ans++;
+                }
+                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
+                    ans++;
+                }
+            } else {
+                leftSum += nums[i];
+                rightSum -= nums[i];
             }
         }
-        for (int j : nums) if (j != 0) return false;
-        return true;
+        return ans;
     }
 };
