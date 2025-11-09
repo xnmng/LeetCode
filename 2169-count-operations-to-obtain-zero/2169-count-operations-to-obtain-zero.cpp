55a0ef224f37dfd17cebbf420a3a2c6b29ec2d32
xnmng <48902714+xnmng@users.noreply.github.com>
2025-11-09 10:16:15 +0800

    Time: 0 ms (100%), Space: 8 MB (40.22%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        if (num1 == 0 || num2 == 0) return 0;
-        return 1 + (num1 >= num2 
-                    ? countOperations(num1 - num2, num2) 
-                    : countOperations(num1, num2 - num1));
+        int ans = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) num1 -= num2;
+            else num2 -= num1;
+            ++ans;
+        }
+        return ans;
     }
+
+    // int countOperations(int num1, int num2) {
+    //     if (num1 == 0 || num2 == 0) return 0;
+    //     return 1 + (num1 >= num2 
+    //                 ? countOperations(num1 - num2, num2) 
+    //                 : countOperations(num1, num2 - num1));
+    // }
 };
