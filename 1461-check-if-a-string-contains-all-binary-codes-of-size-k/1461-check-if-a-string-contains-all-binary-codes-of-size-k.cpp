a93674d9c6381ca4089e510916ab1fa1bb42ebeb
xnmng <48902714+xnmng@users.noreply.github.com>
2026-02-23 09:24:59 +0800

    Time: 22 ms (90.04%), Space: 23.4 MB (92.57%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        unordered_set<int> set;
-        int cur = 0;
+        // unordered_set<int> set;
+        int count = 0;
         int offset = pow(2, k);
+        vector<bool> set(offset, false);
+        int cur = 0;
         int n = s.size();
         for (int i = 0; i < n; ++i) {
             cur *= 2;
             cur += s[i] - '0';
             if (i >= k-1) {
                 if (cur >= offset) cur -= offset; // remove largest bit if it is set
-                set.insert(cur);
+                // set.insert(cur);
+                if (!set[cur]) {
+                    ++count;
+                    set[cur] = true;
+                }
             }
         }
-        return set.size() == offset; // reuse offset as expected count
+        // return set.size() == offset; // reuse offset as expected count
+        return count == offset; // reuse offset as expected count
     }
 };
