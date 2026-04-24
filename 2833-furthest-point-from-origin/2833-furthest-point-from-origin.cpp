06be1d048e012fba0a9fb8bedfe2cfa081e6a35d
xnmng <48902714+xnmng@users.noreply.github.com>
2026-04-24 10:14:35 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // best answer is when all '_' are either all 'L' or all 'R'
+    // since it is symmetrical, just figure out number of '_', 
+    //     and the abs. max value after applying all non-'_'(s)
+    // 
+    // O(n) time, O(1) space
     int furthestDistanceFromOrigin(string moves) {
         int count = 0;
         int left = 0;
