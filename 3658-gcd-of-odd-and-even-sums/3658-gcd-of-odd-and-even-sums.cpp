03b2ee85f8fc502c313a6d3842bf628182c12bda
xnmng <48902714+xnmng@users.noreply.github.com>
2026-07-15 15:10:55 +0800

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // sum of all odd numbers is n^2
+    // sum of all even numbers is n(n+1)
+    // 
+    // 2 consecutive integers are coprime (gcd(n, n+1) = 1)
+    // gcd(n^2, n(n+1)) = n * gcd(n, n+1) = n * 1 = n
     int gcdOfOddEvenSums(int n) {
         return n;
     }
